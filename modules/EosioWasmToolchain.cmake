84c54035f69cfc2a2e424780fe883beae3e7d43c
Bucky Kittinger <larrykittinger@gmail.com>
2018-06-15 18:48:50 -0400

    Changes to installation

--- modules/EosioWasmToolchain.cmake
+++ modules/EosioWasmToolchain.cmake
@@ -1,7 +1,14 @@
-message(STATUS " -- setting up Eosio Wasm Toolchain")
+message(STATUS "Setting up Eosio Wasm Toolchain")
 set(CMAKE_SYSTEM_NAME WebAssembly)
-set(WASM_ROOT "${CMAKE_BINARY_DIR}/EosioClang-prefix/src/EosioClang-build")
-set(EOSIO_APPLY_LIB "${WASM_ROOT}/lib/LLVMEosioApply.dylib")
+set(WASM_ROOT "${CMAKE_INSTALL_PREFIX}")
+set(EOSIO_APPLY_LIB "${WASM_ROOT}/lib/LLVMEosioApply${CMAKE_SHARED_LIBRARY_SUFFIX}")
+
+if(WASM_SDK_BUILD)
+   set(TMP_ROOT "${CMAKE_CURRENT_SOURCE_DIR}")
+else(WASM_SDK_BUILD)
+   set(TMP_ROOT "${WASM_ROOT}")
+endif(WASM_SDK_BUILD)
+
 
 set(CMAKE_C_COMPILER "${WASM_ROOT}/bin/clang")
 set(CMAKE_CXX_COMPILER "${WASM_ROOT}/bin/clang++")
@@ -15,7 +22,7 @@ set(CMAKE_C_FLAGS " ${STD_FLAGS} -O3")
 set(CMAKE_CXX_FLAGS " -std=c++14 -O3 -fno-rtti -fno-exceptions ${STD_FLAGS}")
 
 set(WASM_LINKER "${WASM_ROOT}/bin/wasm-ld")
-set(WASM_IMPORTS "${CMAKE_SOURCE_DIR}/eosio.imports")
+set(WASM_IMPORTS "${TMP_ROOT}/eosio.imports")
 
 set(CMAKE_C_LINK_EXECUTABLE "${WASM_LINKER} <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
 set(CMAKE_CXX_LINK_EXECUTABLE "${WASM_LINKER} <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
@@ -25,13 +32,17 @@ set(CMAKE_RANLIB "${WASM_ROOT}/bin/llvm-ranlib" CACHE PATH "ranlib" FORCE)
 
 set(CMAKE_EXE_LINKER_FLAGS "--allow-undefined-file=${WASM_IMPORTS} -e apply --lto-O3 --gc-sections --merge-data-segments --strip-all -stack-first -zstack-size=8192 -mllvm -use-cfl-aa-in-codegen=both")
 
-set(BOOST_INSTALL_DIR "/usr/local/include" CACHE STRING "Boost include directory")
+find_package(Boost 1.67 REQUIRED)
+#set(BOOST_INSTALL_DIR "/usr/local/include" CACHE STRING "Boost include directory")
 
-set(STANDARD_INCLUDES "${CMAKE_CURRENT_SOURCE_DIR}/libraries/libc++/libcxx/include"
-                      "${CMAKE_CURRENT_SOURCE_DIR}/libraries/libc/musl/include"
-                      "${CMAKE_CURRENT_SOURCE_DIR}/libraries/eosiolib"
-                      "${CMAKE_CURRENT_SOURCE_DIR}/libraries/pfr/include"
-                      "${BOOST_INSTALL_DIR}")
-
+if(WASM_SDK_BUILD)
+   set(STANDARD_INCLUDES "${CMAKE_CURRENT_SOURCE_DIR}/libraries/libc++/libcxx/include"
+                         "${CMAKE_CURRENT_SOURCE_DIR}/libraries/libc/musl/include"
+                         "${CMAKE_CURRENT_SOURCE_DIR}/libraries/eosiolib"
+                         "${CMAKE_CURRENT_SOURCE_DIR}/libraries/pfr/include"
+                         "${Boost_INCLUDE_DIRS}")
+else(WASM_SDK_BUILD)
+   set(STANDARD_INCLUDES "${WASM_ROOT}/include"
+                         "${BOOST_INSTALL_DIR}")
+endif(WASM_SDK_BUILD)
 set(STANDARD_LIBS c++ c eosio)
-
