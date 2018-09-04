4c20a27ef6122d1cf3ac251adbbcf7bf668b3e4a
Bucky Kittinger <larrykittinger@gmail.com>
2018-09-03 22:49:48 -0400

    added optional action/table name attr arg

--- examples/abigen_test/test.cpp
+++ examples/abigen_test/test.cpp
@@ -8,10 +8,12 @@ namespace test {
       float fieldb;
       account_name name;
    };
+
    struct testb {
       void printll() { print("testb"); }
    };
-   struct testc : testb {
+
+   struct [[ eosio::action("testc") ]] test_c : testb {
       uint64_t num;
    };
 }
@@ -20,13 +22,13 @@ class test_contract : public eosio::contract {
   public:
       using contract::contract;
 
-      [[ eosio::action ]]
-      void testacta( account_name user ) {
+      [[ eosio::action("testacta") ]]
+      void testact_a( account_name user ) {
          print( "Hello, ", name{user} );
       }
 
       [[ eosio::action ]]
-      void testactb( test::testc input ) {
+      void testactb( test::test_c input ) {
          print(input.num);
       }
 
@@ -36,9 +38,16 @@ class test_contract : public eosio::contract {
          uint64_t third;
          uint64_t primary_key() const { return owner; }
       };
+      struct [[eosio::table("testtab2")]] test_table2 {
+         uint64_t owner;
+         uint64_t sec;
+         uint64_t third;
+         uint64_t primary_key() const { return owner; }
+      };
 };
 
 typedef eosio::multi_index< N(testtab), test_contract::testtable > testtable_t;
+typedef eosio::multi_index< N(testtab2), test_contract::test_table2 > testtable2_t; // note that the N(testtabb) is the same as the explicit name given to eosio::table
 
 // NOTE THIS IS A TEST CONTRACT AND WILL NOT WORK CORRECTLY, the action `testa` will not be in the dispatcher of this macro
-EOSIO_ABI( test_contract, (testacta)(testactb) )
+EOSIO_ABI( test_contract, (testact_a)(testactb) )
