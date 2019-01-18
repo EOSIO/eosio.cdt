c3b1b87b98e7f7004881ae6fe97ceb97c8a8d3b5
Nathan Pierce <nathan.pierce@block.one>
2019-01-18 14:22:10 -0500

    new build scripts

--- build.sh
+++ build.sh
@@ -1,120 +1,198 @@
-#! /bin/bash
+#!/bin/bash
 
-printf "\t=========== Building eosio.cdt ===========\n\n"
+printf "=========== eosio.cdt ===========\n\n"
 
-RED='\033[0;31m'
-NC='\033[0m'
+VERSION=2.0 # Build script version
+CMAKE_BUILD_TYPE=Release
+export DISK_MIN=20
+DOXYGEN=false
+ENABLE_COVERAGE_TESTING=false
+CORE_SYMBOL_NAME="SYS"
+START_MAKE=true
+
+TIME_BEGIN=$( date -u +%s )
 txtbld=$(tput bold)
 bldred=${txtbld}$(tput setaf 1)
 txtrst=$(tput sgr0)
 
-export DISK_MIN=10
-export TEMP_DIR="/tmp"
-TEMP_DIR='/tmp'
-DISK_MIN=10
+export SRC_LOCATION=${HOME}/src
+export OPT_LOCATION=${HOME}/opt
+export VAR_LOCATION=${HOME}/var
+export ETC_LOCATION=${HOME}/etc
+export BIN_LOCATION=${HOME}/bin
+export DATA_LOCATION=${HOME}/data
+export CMAKE_VERSION_MAJOR=3
+export CMAKE_VERSION_MINOR=13
+export CMAKE_VERSION_PATCH=2
+export CMAKE_VERSION=${CMAKE_VERSION_MAJOR}.${CMAKE_VERSION_MINOR}.${CMAKE_VERSION_PATCH}
+export MONGODB_VERSION=3.6.3
+export MONGODB_ROOT=${OPT_LOCATION}/mongodb-${MONGODB_VERSION}
+export MONGODB_CONF=${ETC_LOCATION}/mongod.conf
+export MONGODB_LOG_LOCATION=${VAR_LOCATION}/log/mongodb
+export MONGODB_LINK_LOCATION=${OPT_LOCATION}/mongodb
+export MONGODB_DATA_LOCATION=${DATA_LOCATION}/mongodb
+export MONGO_C_DRIVER_VERSION=1.13.0
+export MONGO_C_DRIVER_ROOT=${SRC_LOCATION}/mongo-c-driver-${MONGO_C_DRIVER_VERSION}
+export MONGO_CXX_DRIVER_VERSION=3.4.0
+export MONGO_CXX_DRIVER_ROOT=${SRC_LOCATION}/mongo-cxx-driver-r${MONGO_CXX_DRIVER_VERSION}
+export BOOST_VERSION_MAJOR=1
+export BOOST_VERSION_MINOR=67
+export BOOST_VERSION_PATCH=0
+export BOOST_VERSION=${BOOST_VERSION_MAJOR}_${BOOST_VERSION_MINOR}_${BOOST_VERSION_PATCH}
+export BOOST_ROOT=${SRC_LOCATION}/boost_${BOOST_VERSION}
+export BOOST_LINK_LOCATION=${OPT_LOCATION}/boost
+export LLVM_VERSION=release_40
+export LLVM_ROOT=${OPT_LOCATION}/llvm
+export LLVM_DIR=${LLVM_ROOT}/lib/cmake/llvm
+export DOXYGEN_VERSION=1_8_14
+export DOXYGEN_ROOT=${SRC_LOCATION}/doxygen-${DOXYGEN_VERSION}
+export TINI_VERSION=0.18.0
+
+export PATH=$HOME/bin:$PATH:$HOME/opt/llvm/bin
+
+# Setup directories
+mkdir -p $SRC_LOCATION
+mkdir -p $OPT_LOCATION
+mkdir -p $VAR_LOCATION
+mkdir -p $BIN_LOCATION
+mkdir -p $VAR_LOCATION/log
+mkdir -p $ETC_LOCATION
+mkdir -p $MONGODB_LOG_LOCATION
+mkdir -p $MONGODB_DATA_LOCATION
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [ "${CURRENT_DIR}" == "${PWD}" ]; then
+   BUILD_DIR="${PWD}/build"
+else
+   BUILD_DIR="${PWD}"
+fi
 
 # Use current directory's tmp directory if noexec is enabled for /tmp
 if (mount | grep "/tmp " | grep --quiet noexec); then
-  mkdir -p $SOURCE_DIR/tmp
-  TEMP_DIR="${SOURCE_DIR}/tmp"
-  rm -rf $SOURCE_DIR/tmp/*
+      mkdir -p $CURRENT_DIR/tmp
+      TEMP_DIR="${CURRENT_DIR}/tmp"
+      rm -rf $CURRENT_DIR/tmp/*
 else # noexec wasn't found
-  TEMP_DIR="/tmp"
+      TEMP_DIR="/tmp"
 fi
 
-unamestr=`uname`
-if [[ "${unamestr}" == 'Darwin' ]]; then
-   BOOST=/usr/local
-   CXX_COMPILER=g++
-   export ARCH="Darwin"
-   bash ./scripts/eosio_build_darwin.sh
-else
-   OS_NAME=$( cat /etc/os-release | grep ^NAME | cut -d'=' -f2 | sed 's/\"//gI' )
+if [ ! -d "${CURRENT_DIR}/.git" ]; then
+   printf "\\nThis build script only works with sources cloned from git\\n"
+   printf "Please clone a new eos directory with 'git clone https://github.com/EOSIO/eos --recursive'\\n"
+   printf "See the wiki for instructions: https://github.com/EOSIO/eos/wiki\\n"
+   exit 1
+fi
 
+pushd "${CURRENT_DIR}" &> /dev/null
+
+STALE_SUBMODS=$(( $(git submodule status --recursive | grep -c "^[+\-]") ))
+if [ $STALE_SUBMODS -gt 0 ]; then
+   printf "\\ngit submodules are not up to date.\\n"
+   printf "Please run the command 'git submodule update --init --recursive'.\\n"
+   exit 1
+fi
+
+printf "Beginning build version: %s\\n" "${VERSION}"
+printf "%s\\n" "$( date -u )"
+printf "User: %s\\n" "$( whoami )"
+# printf "git head id: %s\\n" "$( cat .git/refs/heads/master )"
+printf "Current branch: %s\\n" "$( git rev-parse --abbrev-ref HEAD )"
+
+ARCH=$( uname )
+printf "\\nARCHITECTURE: %s\\n" "${ARCH}"
+
+popd &> /dev/null
+
+if [ "$ARCH" == "Linux" ]; then
+   export OS_NAME=$( cat /etc/os-release | grep ^NAME | cut -d'=' -f2 | sed 's/\"//gI' )
    case "$OS_NAME" in
-      "Amazon Linux AMI")
-         export ARCH="Amazon Linux AMI"
-         bash ./scripts/eosio_build_amazon.sh
-         ;;
+      "Amazon Linux AMI"|"Amazon Linux")
+         FILE="${CURRENT_DIR}/scripts/eosio_build_amazon.sh"
+         CXX_COMPILER=g++
+         C_COMPILER=gcc
+      ;;
       "CentOS Linux")
-         export ARCH="Centos"
-         export CMAKE=${HOME}/opt/cmake/bin/cmake
-         bash ./scripts/eosio_build_centos.sh
-         ;;
+         FILE="${CURRENT_DIR}/scripts/eosio_build_centos.sh"
+         CXX_COMPILER=g++
+         C_COMPILER=gcc
+      ;;
       "elementary OS")
-         export ARCH="elementary OS"
-         bash ./scripts/eosio_build_ubuntu.sh
-         ;;
+         FILE="${CURRENT_DIR}/scripts/eosio_build_ubuntu.sh"
+         CXX_COMPILER=clang++-4.0
+         C_COMPILER=clang-4.0
+      ;;
       "Fedora")
-         export ARCH="Fedora"
-         bash ./scripts/eosio_build_fedora.sh
-         ;;
+         FILE="${CURRENT_DIR}/scripts/eosio_build_fedora.sh"
+         CXX_COMPILER=g++
+         C_COMPILER=gcc
+      ;;
       "Linux Mint")
-         export ARCH="Linux Mint"
-         bash ./scripts/eosio_build_ubuntu.sh
-         ;;
+         FILE="${CURRENT_DIR}/scripts/eosio_build_ubuntu.sh"
+         CXX_COMPILER=clang++-4.0
+         C_COMPILER=clang-4.0
+      ;;
       "Ubuntu")
-         export ARCH="Ubuntu"
-         bash ./scripts/eosio_build_ubuntu.sh
-         ;;
+         FILE="${CURRENT_DIR}/scripts/eosio_build_ubuntu.sh"
+         CXX_COMPILER=clang++-4.0
+         C_COMPILER=clang-4.0
+      ;;
       "Debian GNU/Linux")
-         export ARCH="Debian"
-	 bash ./scripts/eosio_build_ubuntu.sh
-	 ;;
+         FILE="${CURRENT_DIR}/scripts/eosio_build_ubuntu.sh"
+         CXX_COMPILER=clang++-4.0
+         C_COMPILER=clang-4.0
+      ;;
       *)
-         printf "\\n\\tUnsupported Linux Distribution. Exiting now.\\n\\n"
+         printf "\\nUnsupported Linux Distribution. Exiting now.\\n\\n"
          exit 1
    esac
+   . "$FILE" # Execute OS specific build file
 fi
 
-if [[ `uname` == 'Darwin' ]]; then
+if [ "$ARCH" == "Darwin" ]; then
    FREE_MEM=`vm_stat | grep "Pages free:"`
    read -ra FREE_MEM <<< "$FREE_MEM"
    FREE_MEM=$((${FREE_MEM[2]%?}*(4096))) # free pages * page size
 else
-   FREE_MEM=`LC_ALL=C free | grep "Mem:" | awk '{print $4}'`
+   FREE_MEM=`LANG=C free | grep "Mem:" | awk '{print $4}'`
 fi
-
 CORES_AVAIL=`getconf _NPROCESSORS_ONLN`
 MEM_CORES=$(( ${FREE_MEM}/4000000 )) # 4 gigabytes per core
 MEM_CORES=$(( $MEM_CORES > 0 ? $MEM_CORES : 1 ))
 CORES=$(( $CORES_AVAIL < $MEM_CORES ? $CORES_AVAIL : $MEM_CORES ))
 
-#check submodules
-if [ $(( $(git submodule status --recursive | grep -c "^[+\-]") )) -gt 0 ]; then
-   printf "\\n\\tgit submodules are not up to date.\\n"
-   printf "\\tPlease run the command 'git submodule update --init --recursive'.\\n"
-   exit 1
+mkdir -p $BUILD_DIR
+pushd $BUILD_DIR &> /dev/null
+
+if [ -z "${CMAKE}" ]; then
+  CMAKE=$( command -v cmake 2>/dev/null )
 fi
 
-mkdir -p build
-pushd build &> /dev/null
+printf "\\n========================================================================\\n"
+printf "======================= Starting EOSIO.CDT Build =======================\\n"
 
-if [ -z "$CMAKE" ]; then
-  CMAKE=$( command -v cmake )
-fi
-
-"$CMAKE" -DCMAKE_INSTALL_PREFIX=/usr/local/eosio.cdt ../
-if [ $? -ne 0 ]; then
-   exit -1;
-fi
-make -j${CORES}
-if [ $? -ne 0 ]; then
-   exit -1;
-fi
+$CMAKE -DCMAKE_INSTALL_PREFIX=$OPT_LOCATION/eosio.cdt ../
+if [ $? -ne 0 ]; then exit -1; fi
+make -j$CORES
+if [ $? -ne 0 ]; then exit -1; fi
 popd &> /dev/null
 
-printf "\n${bldred}\t      ___           ___           ___                       ___\n"
-printf "\t     /  /\\         /  /\\         /  /\\        ___          /  /\\ \n"
-printf "\t    /  /:/_       /  /::\\       /  /:/_      /  /\\        /  /::\\ \n"
-printf "\t   /  /:/ /\\     /  /:/\\:\\     /  /:/ /\\    /  /:/       /  /:/\\:\\ \n"
-printf "\t  /  /:/ /:/_   /  /:/  \\:\\   /  /:/ /::\\  /__/::\\      /  /:/  \\:\\ \n"
-printf "\t /__/:/ /:/ /\\ /__/:/ \\__\\:\\ /__/:/ /:/\\:\\ \\__\\/\\:\\__  /__/:/ \\__\\:\\ \n"
-printf "\t \\  \\:\\/:/ /:/ \\  \\:\\ /  /:/ \\  \\:\\/:/~/:/    \\  \\:\\/\\ \\  \\:\\ /  /:/ \n"
-printf "\t  \\  \\::/ /:/   \\  \\:\\  /:/   \\  \\::/ /:/      \\__\\::/  \\  \\:\\  /:/ \n"
-printf "\t   \\  \\:\\/:/     \\  \\:\\/:/     \\__\\/ /:/       /__/:/    \\  \\:\\/:/ \n"
-printf "\t    \\  \\::/       \\  \\::/        /__/:/        \\__\\/      \\  \\::/ \n"
-printf "\t     \\__\\/         \\__\\/         \\__\\/                     \\__\\/ \n${txtrst}"
+TIME_END=$(( $(date -u +%s) - $TIME_BEGIN ))
 
-printf "\\tFor more information:\\n"
-printf "\\tEOSIO website: https://eos.io\\n"
+printf "\n\n _______  _______  _______ _________ _______\n"
+printf '(  ____ \(  ___  )(  ____ \\\\__   __/(  ___  )\n'
+printf "| (    \/| (   ) || (    \/   ) (   | (   ) |\n"
+printf "| (__    | |   | || (_____    | |   | |   | |\n"
+printf "|  __)   | |   | |(_____  )   | |   | |   | |\n"
+printf "| (      | |   | |      ) |   | |   | |   | |\n"
+printf "| (____/\| (___) |/\____) |___) (___| (___) |\n"
+printf "(_______/(_______)\_______)\_______/(_______)\n\n"
+
+printf "\\nEOSIO.CDT has been successfully built. %02d:%02d:%02d\\n\\n" $(($TIME_END/3600)) $(($TIME_END%3600/60)) $(($TIME_END%60))
+printf "${txtrst}==============================================================================================\\n"
+
+printf "For more information:\\n"
+printf "EOSIO website: https://eos.io\\n"
+printf "EOSIO Telegram channel @ https://t.me/EOSProject\\n"
+printf "EOSIO resources: https://eos.io/resources/\\n"
+printf "EOSIO Stack Exchange: https://eosio.stackexchange.com\\n"
+printf "EOSIO wiki: https://github.com/EOSIO/eos/wiki\\n\\n\\n"
--- install.sh
+++ install.sh
@@ -29,85 +29,88 @@
 #
 # https://github.com/EOSIO/eos/blob/master/LICENSE.txt
 ##########################################################################
-   
 
-	CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-	if [ "${CWD}" != "${PWD}" ]; then
-		printf "\\n\\tPlease cd into directory %s to run this script.\\n \\tExiting now.\\n\\n" "${CWD}"
-		exit 1
-	fi
+OPT_LOCATION=$HOME/opt
+BIN_LOCATION=$HOME/bin
+LIB_LOCATION=$HOME/lib
 
-	BUILD_DIR="${PWD}/build"
-	CMAKE_BUILD_TYPE=Release
-	TIME_BEGIN=$( date -u +%s )
-   INSTALL_PREFIX="/usr/local/eosio.cdt"
-	VERSION=1.2
+CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [ "${CWD}" != "${PWD}" ]; then
+   printf "\\n\\tPlease cd into directory %s to run this script.\\n \\tExiting now.\\n\\n" "${CWD}"
+   exit 1
+fi
 
-	txtbld=$(tput bold)
-	bldred=${txtbld}$(tput setaf 1)
-	txtrst=$(tput sgr0)
+BUILD_DIR="${PWD}/build"
+CMAKE_BUILD_TYPE=Release
+TIME_BEGIN=$( date -u +%s )
+INSTALL_PREFIX=$OPT_LOCATION/eosio.cdt
+VERSION=2.0
 
-   create_symlink() {
-      pushd /usr/local/bin &> /dev/null
-      ln -sf ../eosio.cdt/bin/$1 $2
-      popd &> /dev/null
-   }
+txtbld=$(tput bold)
+bldred=${txtbld}$(tput setaf 1)
+txtrst=$(tput sgr0)
 
-   install_symlinks() {
-		printf "\\n\\tInstalling EOSIO.CDT Binary Symlinks\\n\\n"
-      create_symlink "llvm-ranlib eosio-ranlib"
-      create_symlink "llvm-ar eosio-ar"
-      create_symlink "llvm-objdump eosio-objdump"
-      create_symlink "llvm-readelf eosio-readelf"
-      create_symlink "eosio-cc eosio-cc"
-      create_symlink "eosio-cpp eosio-cpp"
-      create_symlink "eosio-ld eosio-ld"
-      create_symlink "eosio-pp eosio-pp"
-      create_symlink "eosio-init eosio-init"
-      create_symlink "eosio-abigen eosio-abigen"
-      create_symlink "eosio-abidiff eosio-abidiff"
-      create_symlink "eosio-wasm2wast eosio-wasm2wast"
-      create_symlink "eosio-wast2wasm eosio-wast2wasm"
-   }
-   
-   create_cmake_symlink() {
-      mkdir -p /usr/local/lib/cmake/eosio.cdt
-      pushd /usr/local/lib/cmake/eosio.cdt &> /dev/null
-      ln -sf ../../../eosio.cdt/lib/cmake/eosio.cdt/$1 $1
-      popd &> /dev/null
-   }
-	if [ ! -d "${BUILD_DIR}" ]; then
-      printf "\\n\\tError, build.sh has not ran.  Please run ./build.sh first!\\n\\n"
-      exit -1
-	fi
+create_symlink() {
+   pushd $BIN_LOCATION &> /dev/null
+   ln -sf ../eosio.cdt/bin/$1 $2
+   popd &> /dev/null
+}
 
-	if ! pushd "${BUILD_DIR}"
-	then
-		printf "Unable to enter build directory %s.\\n Exiting now.\\n" "${BUILD_DIR}"
-		exit 1;
-	fi
-   
-	if ! make install
-	then
-		printf "\\n\\t>>>>>>>>>>>>>>>>>>>> MAKE installing EOSIO has exited with the above error.\\n\\n"
-		exit -1
-	fi
-   popd &> /dev/null 
+install_symlinks() {
+   printf "\\n\\tInstalling EOSIO.CDT Binary Symlinks\\n\\n"
+   create_symlink "llvm-ranlib eosio-ranlib"
+   create_symlink "llvm-ar eosio-ar"
+   create_symlink "llvm-objdump eosio-objdump"
+   create_symlink "llvm-readelf eosio-readelf"
+   create_symlink "eosio-cc eosio-cc"
+   create_symlink "eosio-cpp eosio-cpp"
+   create_symlink "eosio-ld eosio-ld"
+   create_symlink "eosio-pp eosio-pp"
+   create_symlink "eosio-init eosio-init"
+   create_symlink "eosio-abigen eosio-abigen"
+   create_symlink "eosio-abidiff eosio-abidiff"
+   create_symlink "eosio-wasm2wast eosio-wasm2wast"
+   create_symlink "eosio-wast2wasm eosio-wast2wasm"
+}
 
-   install_symlinks   
-   create_cmake_symlink "eosio.cdt-config.cmake"
+create_cmake_symlink() {
+   mkdir -p $LIB_LOCATION/cmake/eosio.cdt
+   pushd $LIB_LOCATION/cmake/eosio.cdt &> /dev/null
+   ln -sf ../../../eosio.cdt/lib/cmake/eosio.cdt/$1 $1
+   popd &> /dev/null
+}
+if [ ! -d "${BUILD_DIR}" ]; then
+   printf "\\n\\tError, build.sh has not ran.  Please run ./build.sh first!\\n\\n"
+   exit -1
+fi
 
-   printf "\n${bldred}\t      ___           ___           ___                       ___\n"
-   printf "\t     /  /\\         /  /\\         /  /\\        ___          /  /\\ \n"
-   printf "\t    /  /:/_       /  /::\\       /  /:/_      /  /\\        /  /::\\ \n"
-   printf "\t   /  /:/ /\\     /  /:/\\:\\     /  /:/ /\\    /  /:/       /  /:/\\:\\ \n"
-   printf "\t  /  /:/ /:/_   /  /:/  \\:\\   /  /:/ /::\\  /__/::\\      /  /:/  \\:\\ \n"
-   printf "\t /__/:/ /:/ /\\ /__/:/ \\__\\:\\ /__/:/ /:/\\:\\ \\__\\/\\:\\__  /__/:/ \\__\\:\\ \n"
-   printf "\t \\  \\:\\/:/ /:/ \\  \\:\\ /  /:/ \\  \\:\\/:/~/:/    \\  \\:\\/\\ \\  \\:\\ /  /:/ \n"
-   printf "\t  \\  \\::/ /:/   \\  \\:\\  /:/   \\  \\::/ /:/      \\__\\::/  \\  \\:\\  /:/ \n"
-   printf "\t   \\  \\:\\/:/     \\  \\:\\/:/     \\__\\/ /:/       /__/:/    \\  \\:\\/:/ \n"
-   printf "\t    \\  \\::/       \\  \\::/        /__/:/        \\__\\/      \\  \\::/ \n"
-   printf "\t     \\__\\/         \\__\\/         \\__\\/                     \\__\\/ \n${txtrst}"
+if ! pushd "${BUILD_DIR}"
+then
+   printf "Unable to enter build directory %s.\\n Exiting now.\\n" "${BUILD_DIR}"
+   exit 1;
+fi
 
-	printf "\\tFor more information:\\n"
-	printf "\\tEOSIO website: https://eos.io\\n"
+if ! make install
+then
+   printf "\\n\\t>>>>>>>>>>>>>>>>>>>> MAKE installing EOSIO has exited with the above error.\\n\\n"
+   exit -1
+fi
+popd &> /dev/null 
+
+install_symlinks   
+create_cmake_symlink "eosio.cdt-config.cmake"
+
+printf "\n${bldred}\t      ___           ___           ___                       ___\n"
+printf "\t     /  /\\         /  /\\         /  /\\        ___          /  /\\ \n"
+printf "\t    /  /:/_       /  /::\\       /  /:/_      /  /\\        /  /::\\ \n"
+printf "\t   /  /:/ /\\     /  /:/\\:\\     /  /:/ /\\    /  /:/       /  /:/\\:\\ \n"
+printf "\t  /  /:/ /:/_   /  /:/  \\:\\   /  /:/ /::\\  /__/::\\      /  /:/  \\:\\ \n"
+printf "\t /__/:/ /:/ /\\ /__/:/ \\__\\:\\ /__/:/ /:/\\:\\ \\__\\/\\:\\__  /__/:/ \\__\\:\\ \n"
+printf "\t \\  \\:\\/:/ /:/ \\  \\:\\ /  /:/ \\  \\:\\/:/~/:/    \\  \\:\\/\\ \\  \\:\\ /  /:/ \n"
+printf "\t  \\  \\::/ /:/   \\  \\:\\  /:/   \\  \\::/ /:/      \\__\\::/  \\  \\:\\  /:/ \n"
+printf "\t   \\  \\:\\/:/     \\  \\:\\/:/     \\__\\/ /:/       /__/:/    \\  \\:\\/:/ \n"
+printf "\t    \\  \\::/       \\  \\::/        /__/:/        \\__\\/      \\  \\::/ \n"
+printf "\t     \\__\\/         \\__\\/         \\__\\/                     \\__\\/ \n${txtrst}"
+
+printf "\\tFor more information:\\n"
+printf "\\tEOSIO website: https://eos.io\\n"
