0a17e8b73ca53294dd36a6ab06e44d8556da8560
Nathan Pierce <nathan.pierce@block.one>
2019-02-07 21:39:18 -0500

    testing

--- build.sh
+++ build.sh
@@ -1,73 +1,137 @@
-#! /bin/bash
+#!/bin/bash
 
-printf "\t=========== Building eosio.cdt ===========\n\n"
+printf "=========== eosio.cdt ===========\n\n"
 
-RED='\033[0;31m'
-NC='\033[0m'
+VERSION=2.0 # Build script version
+CMAKE_BUILD_TYPE=Release
+export DISK_MIN=20
+DOXYGEN=false
+ENABLE_COVERAGE_TESTING=false
+CORE_SYMBOL_NAME="SYS"
+START_MAKE=true
+
+TIME_BEGIN=$( date -u +%s )
 txtbld=$(tput bold)
 bldred=${txtbld}$(tput setaf 1)
 txtrst=$(tput sgr0)
 
-export DISK_MIN=10
-export TEMP_DIR="/tmp"
-TEMP_DIR='/tmp'
-DISK_MIN=10
+export SRC_LOCATION=${HOME}/src
+export OPT_LOCATION=${HOME}/opt
+export VAR_LOCATION=${HOME}/var
+export ETC_LOCATION=${HOME}/etc
+export BIN_LOCATION=${HOME}/bin
+export DATA_LOCATION=${HOME}/data
+export CMAKE_VERSION_MAJOR=3
+export CMAKE_VERSION_MINOR=13
+export CMAKE_VERSION_PATCH=2
+export CMAKE_VERSION=${CMAKE_VERSION_MAJOR}.${CMAKE_VERSION_MINOR}.${CMAKE_VERSION_PATCH}
+export BOOST_VERSION_MAJOR=1
+export BOOST_VERSION_MINOR=67
+export BOOST_VERSION_PATCH=0
+export BOOST_VERSION=${BOOST_VERSION_MAJOR}_${BOOST_VERSION_MINOR}_${BOOST_VERSION_PATCH}
+export BOOST_ROOT=${SRC_LOCATION}/boost_${BOOST_VERSION}
+export BOOST_LINK_LOCATION=${OPT_LOCATION}/boost
+export TINI_VERSION=0.18.0
+
+export PATH=$HOME/bin:$PATH:$HOME/opt/llvm/bin
+
+# Setup directories
+mkdir -p $SRC_LOCATION
+mkdir -p $OPT_LOCATION
+mkdir -p $VAR_LOCATION
+mkdir -p $BIN_LOCATION
+mkdir -p $VAR_LOCATION/log
+mkdir -p $ETC_LOCATION
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [ "${CURRENT_DIR}" == "${PWD}" ]; then
+   BUILD_DIR="${PWD}/build"
+else
+   BUILD_DIR="${PWD}"
+fi
 
 # Use current directory's tmp directory if noexec is enabled for /tmp
 if (mount | grep "/tmp " | grep --quiet noexec); then
-  mkdir -p $SOURCE_DIR/tmp
-  TEMP_DIR="${SOURCE_DIR}/tmp"
-  rm -rf $SOURCE_DIR/tmp/*
+      mkdir -p $CURRENT_DIR/tmp
+      TEMP_DIR="${CURRENT_DIR}/tmp"
+      rm -rf $CURRENT_DIR/tmp/*
 else # noexec wasn't found
-  TEMP_DIR="/tmp"
+      TEMP_DIR="/tmp"
 fi
 
-unamestr=`uname`
-if [[ "${unamestr}" == 'Darwin' ]]; then
-   BOOST=/usr/local
-   CXX_COMPILER=g++
-   export ARCH="Darwin"
-   bash ./scripts/eosio_build_darwin.sh
-else
-   OS_NAME=$( cat /etc/os-release | grep ^NAME | cut -d'=' -f2 | sed 's/\"//gI' )
+if [ ! -d "${CURRENT_DIR}/.git" ]; then
+   printf "\\nThis build script only works with sources cloned from git\\n"
+   printf "Please clone a new eos directory with 'git clone https://github.com/EOSIO/eos --recursive'\\n"
+   printf "See the wiki for instructions: https://github.com/EOSIO/eos/wiki\\n"
+   exit 1
+fi
 
+pushd "${CURRENT_DIR}" &> /dev/null
+
+STALE_SUBMODS=$(( $(git submodule status --recursive | grep -c "^[+\-]") ))
+if [ $STALE_SUBMODS -gt 0 ]; then
+   printf "\\ngit submodules are not up to date.\\n"
+   printf "Please run the command 'git submodule update --init --recursive'.\\n"
+   exit 1
+fi
+
+printf "Beginning build version: %s\\n" "${VERSION}"
+printf "%s\\n" "$( date -u )"
+printf "User: %s\\n" "$( whoami )"
+# printf "git head id: %s\\n" "$( cat .git/refs/heads/master )"
+printf "Current branch: %s\\n" "$( git rev-parse --abbrev-ref HEAD )"
+
+ARCH=$( uname )
+printf "\\nARCHITECTURE: %s\\n" "${ARCH}"
+
+popd &> /dev/null
+
+if [ "$ARCH" == "Linux" ]; then
+   export OS_NAME=$( cat /etc/os-release | grep ^NAME | cut -d'=' -f2 | sed 's/\"//gI' )
    case "$OS_NAME" in
-      "Amazon Linux AMI")
-         export ARCH="Amazon Linux AMI"
-         bash ./scripts/eosio_build_amazon.sh
-         ;;
+      "Amazon Linux AMI"|"Amazon Linux")
+         FILE="${CURRENT_DIR}/scripts/eosio_build_amazon.sh"
+         CXX_COMPILER=g++
+         C_COMPILER=gcc
+      ;;
       "CentOS Linux")
-         export ARCH="Centos"
-         export CMAKE=${HOME}/opt/cmake/bin/cmake
-         bash ./scripts/eosio_build_centos.sh
-         ;;
+         FILE="${CURRENT_DIR}/scripts/eosio_build_centos.sh"
+         CXX_COMPILER=g++
+         C_COMPILER=gcc
+      ;;
       "elementary OS")
-         export ARCH="elementary OS"
-         bash ./scripts/eosio_build_ubuntu.sh
-         ;;
+         FILE="${CURRENT_DIR}/scripts/eosio_build_ubuntu.sh"
+         CXX_COMPILER=clang++-4.0
+         C_COMPILER=clang-4.0
+      ;;
       "Fedora")
-         export ARCH="Fedora"
-         bash ./scripts/eosio_build_fedora.sh
-         ;;
+         FILE="${CURRENT_DIR}/scripts/eosio_build_fedora.sh"
+         CXX_COMPILER=g++
+         C_COMPILER=gcc
+      ;;
       "Linux Mint")
-         export ARCH="Linux Mint"
-         bash ./scripts/eosio_build_ubuntu.sh
-         ;;
+         FILE="${CURRENT_DIR}/scripts/eosio_build_ubuntu.sh"
+         CXX_COMPILER=clang++-4.0
+         C_COMPILER=clang-4.0
+      ;;
       "Ubuntu")
-         export ARCH="Ubuntu"
-         bash ./scripts/eosio_build_ubuntu.sh
-         ;;
+         FILE="${CURRENT_DIR}/scripts/eosio_build_ubuntu.sh"
+         CXX_COMPILER=clang++-4.0
+         C_COMPILER=clang-4.0
+      ;;
       "Debian GNU/Linux")
-         export ARCH="Debian"
-	 bash ./scripts/eosio_build_ubuntu.sh
-	 ;;
+         FILE="${CURRENT_DIR}/scripts/eosio_build_ubuntu.sh"
+         CXX_COMPILER=clang++-4.0
+         C_COMPILER=clang-4.0
+      ;;
       *)
-         printf "\\n\\tUnsupported Linux Distribution. Exiting now.\\n\\n"
+         printf "\\nUnsupported Linux Distribution. Exiting now.\\n\\n"
          exit 1
    esac
 fi
 
-if [[ `uname` == 'Darwin' ]]; then
+if [ "$ARCH" == "Darwin" ]; then
+   FILE="${CURRENT_DIR}/scripts/eosio_build_darwin.sh"
    FREE_MEM=`vm_stat | grep "Pages free:"`
    read -ra FREE_MEM <<< "$FREE_MEM"
    FREE_MEM=$((${FREE_MEM[2]%?}*(4096))) # free pages * page size
@@ -75,34 +139,32 @@ else
    FREE_MEM=`LC_ALL=C free | grep "Mem:" | awk '{print $4}'`
 fi
 
+pushd $SRC_LOCATION &> /dev/null
+. "$FILE" # Execute OS specific build file
+popd &> /dev/null
+
 CORES_AVAIL=`getconf _NPROCESSORS_ONLN`
 MEM_CORES=$(( ${FREE_MEM}/4000000 )) # 4 gigabytes per core
 MEM_CORES=$(( $MEM_CORES > 0 ? $MEM_CORES : 1 ))
 CORES=$(( $CORES_AVAIL < $MEM_CORES ? $CORES_AVAIL : $MEM_CORES ))
 
-#check submodules
-if [ $(( $(git submodule status --recursive | grep -c "^[+\-]") )) -gt 0 ]; then
-   printf "\\n\\tgit submodules are not up to date.\\n"
-   printf "\\tPlease run the command 'git submodule update --init --recursive'.\\n"
-   exit 1
+mkdir -p $BUILD_DIR
+pushd $BUILD_DIR &> /dev/null
+
+if [ -z "${CMAKE}" ]; then
+  CMAKE=$( command -v cmake 2>/dev/null )
 fi
 
-mkdir -p build
-pushd build &> /dev/null
+printf "\\n========================================================================\\n"
+printf "======================= Starting EOSIO.CDT Build =======================\\n"
 
-if [ -z "$CMAKE" ]; then
-  CMAKE=$( command -v cmake )
-fi
-
-"$CMAKE" -DCMAKE_INSTALL_PREFIX=/usr/local/eosio.cdt ../
-if [ $? -ne 0 ]; then
-   exit -1;
-fi
-make -j${CORES}
-if [ $? -ne 0 ]; then
-   exit -1;
-fi
+$CMAKE -DCMAKE_INSTALL_PREFIX=$OPT_LOCATION/eosio.cdt "${CURRENT_DIR}"
+if [ $? -ne 0 ]; then exit -1; fi
+make -j$CORES
+if [ $? -ne 0 ]; then exit -1; fi
 popd &> /dev/null
+
+TIME_END=$(( $(date -u +%s) - $TIME_BEGIN ))
 
 printf "\n${bldred}\t      ___           ___           ___                       ___\n"
 printf "\t     /  /\\         /  /\\         /  /\\        ___          /  /\\ \n"
@@ -116,5 +178,12 @@ printf "\t   \\  \\:\\/:/     \\  \\:\\/:/     \\__\\/ /:/       /__/:/    \\  \
 printf "\t    \\  \\::/       \\  \\::/        /__/:/        \\__\\/      \\  \\::/ \n"
 printf "\t     \\__\\/         \\__\\/         \\__\\/                     \\__\\/ \n${txtrst}"
 
-printf "\\tFor more information:\\n"
-printf "\\tEOSIO website: https://eos.io\\n"
+printf "\\nEOSIO.CDT has been successfully built. %02d:%02d:%02d\\n\\n" $(($TIME_END/3600)) $(($TIME_END%3600/60)) $(($TIME_END%60))
+printf "${txtrst}==============================================================================================\\n"
+
+printf "For more information:\\n"
+printf "EOSIO website: https://eos.io\\n"
+printf "EOSIO Telegram channel @ https://t.me/EOSProject\\n"
+printf "EOSIO resources: https://eos.io/resources/\\n"
+printf "EOSIO Stack Exchange: https://eosio.stackexchange.com\\n"
+printf "EOSIO wiki: https://github.com/EOSIO/eos/wiki\\n\\n\\n"
--- uninstall.sh
+++ uninstall.sh
@@ -1,19 +1,23 @@
 #! /bin/bash
 
-binaries=(eosio-ranlib
-          eosio-ar
-          eosio-objdump
-          eosio-readelf
-          eosio-abigen
-          eosio-wasm2wast
-          eosio-wast2wasm
-          eosio-pp
-          eosio-cc
-          eosio-cpp
-          eosio-ld)
+OPT_LOCATION=$HOME/opt
+
+binaries=(
+   eosio-ranlib
+   eosio-ar
+   eosio-objdump
+   eosio-readelf
+   eosio-abigen
+   eosio-wasm2wast
+   eosio-wast2wasm
+   eosio-pp
+   eosio-cc
+   eosio-cpp
+   eosio-ld
+)
 
 if [ -d "/usr/local/eosio.cdt" ]; then
-   printf "\tDo you wish to remove this install? (requires sudo)\n"
+   printf "Do you wish to remove this install? (requires sudo)\n"
    select yn in "Yes" "No"; do
       case $yn in
          [Yy]* )
@@ -29,6 +33,35 @@ if [ -d "/usr/local/eosio.cdt" ]; then
             popd &> /dev/null
             break;;
          [Nn]* ) 
+            printf "Aborting uninstall\n\n"
+            exit -1;;
+      esac
+   done
+fi
+
+if [ -d $OPT_LOCATION/eosio.cdt ]; then
+   printf "Do you wish to remove this install? (requires sudo)\n"
+   select yn in "Yes" "No"; do
+      case $yn in
+         [Yy]* )
+            if [ "$(id -u)" -ne 0 ]; then
+               printf "\nThis requires sudo, please run ./uninstall.sh with sudo\n\n"
+               exit -1
+            fi
+            pushd $HOME &> /dev/null
+            pushd opt &> /dev/null
+            rm -rf eosio.cdt
+            popd &> /dev/null
+            pushd bin &> /dev/null
+            for binary in ${binaries[@]}; do
+               rm ${binary}
+            done
+            popd &> /dev/null
+            pushd lib/cmake &> /dev/null
+            rm -rf eosio.cdt
+            popd &> /dev/null
+            break;;
+         [Nn]* )
             printf "\tAborting uninstall\n\n"
             exit -1;;
       esac
@@ -36,7 +69,7 @@ if [ -d "/usr/local/eosio.cdt" ]; then
 fi
 
 if [ -d "/usr/local/eosio.wasmsdk" ]; then
-   printf "\tDo you wish to remove this install? (requires sudo)\n"
+   printf "Do you wish to remove this install? (requires sudo)\n"
    select yn in "Yes" "No"; do
       case $yn in
          [Yy]* )
@@ -50,7 +83,7 @@ if [ -d "/usr/local/eosio.wasmsdk" ]; then
             break;;
 
          [Nn]* ) 
-            printf "\tAborting uninstall\n\n"
+            printf "Aborting uninstall\n\n"
             exit -1;;
       esac
    done
