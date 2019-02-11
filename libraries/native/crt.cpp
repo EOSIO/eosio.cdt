e1deefb134063e29b09cbfb9431e3fa7154d6bea
johndebord <john.debord@block.one>
2019-02-11 18:25:53 -0500

    Fix for `__bzero` behavior

--- libraries/native/crt.cpp
+++ libraries/native/crt.cpp
@@ -134,6 +134,7 @@ extern "C" {
 
    extern "C" void* memset(void*, int, size_t);
    extern "C" void __bzero(void* to, size_t cnt) {
-      memset(to, 0, cnt);
+      char* cp{static_cast<char*>(to)};
+      while (cnt--) *cp++ = 0;
    }
 }
