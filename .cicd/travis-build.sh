fb40b0b6bf27e3bea23aeedecec44811a3b8843e
Scott Arnette <scott.a.arnette@gmail.com>
2019-07-18 13:50:12 -0400

    Changed docker run command.

--- .cicd/travis-build.sh
+++ .cicd/travis-build.sh
@@ -6,10 +6,10 @@ function build() {
     [[ $IMAGE_TAG == centos-7 ]] && PRECOMMANDS="source /opt/rh/python33/enable && source /opt/rh/devtoolset-7/enable &&"
     [[ $IMAGE_TAG == amazonlinux-2 || $IMAGE_TAG == centos-7 ]] && EXPORTS="export PATH=/usr/lib64/ccache:$PATH &&" || EXPORTS="$EXPORTS export PATH=/usr/lib/ccache:$PATH &&" # ccache needs to come first in the list
     # DOCKER
-    docker run --rm -v $(pwd):/eosio.cdt -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache eosio/producer:ci-${IMAGE_TAG}-cdt bash -c " \
-        $PRE_COMMANDS ccache -s && \
-        mkdir /eosio.cdt/build && cd /eosio.cdt/build && $EXPORTS cmake -DCMAKE_CXX_COMPILER_LAUNCHER=ccache /eosio.cdt && make -j $(getconf _NPROCESSORS_ONLN)
-        ctest -j $CPU_CORES -L unit_tests -V -T Test"
+    docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache eosio/producer:ci-${IMAGE_TAG}-cdt bash -c " \
+        rm -f /workdir/modules/ClangExternalProject.txt && ln -s /tmp/ClangExternalProject.txt /workdir/modules/ClangExternalProject.txt && \
+        mkdir /workdir/build && cd /workdir/build && ln -s /opt/eosio.cdt/build/eosio_llvm/ /workdir/build/eosio_llvm && cmake .. && make -j $(getconf _NPROCESSORS_ONLN) && \
+        ctest -j$(getconf _NPROCESSORS_ONLN) -L unit_tests -V -T Test"
 }
 
 CPU_CORES=$(getconf _NPROCESSORS_ONLN)
