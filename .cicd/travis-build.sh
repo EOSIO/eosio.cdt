19deb26ab058851f0993c77eb185f22832e79a20
Scott Arnette <scott.a.arnette@gmail.com>
2019-07-18 16:00:15 -0400

    Added latest hashes for buildkite.

--- .cicd/travis-build.sh
+++ .cicd/travis-build.sh
@@ -3,15 +3,6 @@ set -eo pipefail
 cd $( dirname "${BASH_SOURCE[0]}" )/.. # Ensure we're in the repo root and not inside of scripts
 . ./.cicd/.helpers
 
-function build() {
-    # Per distro additions to docker command
-    # DOCKER
-    docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache ${FULL_TAG} bash -c " $PRECOMMANDS && \
-        rm -f /workdir/modules/ClangExternalProject.txt && ln -s /tmp/ClangExternalProject.txt /workdir/modules/ClangExternalProject.txt && \
-        mkdir /workdir/build && cd /workdir/build && ln -s /opt/eosio.cdt/build/eosio_llvm/ /workdir/build/eosio_llvm && cmake .. && make -j $(getconf _NPROCESSORS_ONLN) && \
-        ctest -j$(getconf _NPROCESSORS_ONLN) -L unit_tests -V -T Test"
-}
-
 CPU_CORES=$(getconf _NPROCESSORS_ONLN)
 
 if [[ "$(uname)" == Darwin ]]; then
