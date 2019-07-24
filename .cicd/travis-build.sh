32652893584d25b6cc5558c0a9f654e40f242cb2
Scott Arnette <scott.a.arnette@gmail.com>
2019-07-24 15:25:41 -0400

    Updated travis_wait times.

--- .cicd/travis-build.sh
+++ .cicd/travis-build.sh
@@ -11,15 +11,15 @@ if [[ "$(uname)" == Darwin ]]; then
     echo '$ cmake ..'
     cmake ..
     echo "$ make -j$MAKE_PROC_LIMIT"
-    travis_wait 120 make -j$MAKE_PROC_LIMIT
+    travis_wait 150 make -j$MAKE_PROC_LIMIT
     ctest -j$MAKE_PROC_LIMIT -L unit_tests -V -T Test
 else # linux
     echo 'Detected Linux, building in Docker.'
     # Testing new core counts.
     if [[ $IMAGE_TAG == "ubuntu-18.04" ]]; then
         echo $($MAKE_PROC_LIMIT)
-        travis_wait 120 execute docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache -e MAKE_PROC_LIMIT eosio/producer:ci-ubuntu-18.04
+        travis_wait 40 execute docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache -e MAKE_PROC_LIMIT eosio/producer:ci-ubuntu-18.04
     else
-        travis_wait 120 execute docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache ${FULL_TAG}
+        travis_wait 40 execute docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache ${FULL_TAG}
     fi
 fi
