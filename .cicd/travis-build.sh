1cd188e0cfaf33c05fca9c593f438608bf67776b
Scott Arnette <scott.a.arnette@gmail.com>
2019-07-25 15:45:58 -0400

    Timeouts are random and extending via travis_wait is ineffective. Replaced with travis_retry.

--- .cicd/travis-build.sh
+++ .cicd/travis-build.sh
@@ -15,6 +15,5 @@ if [[ "$(uname)" == Darwin ]]; then
     travis_wait 30 ctest -j$MAKE_PROC_LIMIT -L unit_tests -V -T Test
 else # linux
     echo 'Detected Linux, building in Docker.'
-    # Testing new core counts.
-    travis_wait 40 execute docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e MAKE_PROC_LIMIT -e CCACHE_DIR=/opt/.ccache ${FULL_TAG}
+    execute docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e MAKE_PROC_LIMIT -e CCACHE_DIR=/opt/.ccache ${FULL_TAG}
 fi
