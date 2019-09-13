cdd4fa693a6707b13f4951e66ab2f9f4a724f2f1
root <root@ben-k8s-agents-docsgen-0.ben-k8s-agents-docsgen.default.svc.cluster.local>
2019-09-13 23:43:14 +0000

    Generate: v1.6.2

--- libraries/eosiolib/public_key.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <eosiolib/crypto.hpp>
-
-// This file only exists so that existing contracts that include eosiolib/public_key.hpp do not break.
-// Going forward contracts should instead just include eosiolib/crypto.hpp
