85f116c2966a346b63e731b9ea0b653892893cbc
Nathan Pierce <nathan.pierce@block.one>
2019-12-24 08:37:18 -0500

    quick travis fixes

--- .cicd/toolchain-tests.sh
+++ .cicd/toolchain-tests.sh
@@ -12,6 +12,7 @@ TOOLCHAIN_TESTS_COMMANDS="$PRE_COMMANDS && $TEST"
 
 if [[ $(uname) == 'Darwin' ]]; then
 
+    [[ $TRAVIS != true ]] && tar -xzf build.tar.gz
     # You can't use chained commands in execute
     cd $BUILD_DIR
     bash -c "$TEST"
@@ -21,7 +22,7 @@ else # Linux
     ARGS=${ARGS:-"--rm --init $(buildkite-intrinsics) -v $(pwd):$(pwd)"}
     [[ $TRAVIS == true ]] && ARGS="$ARGS -e JOBS -e CCACHE_DIR=/opt/.ccache" || TOOLCHAIN_TESTS_COMMANDS="cd $(pwd) && tar -xzf build.tar.gz && $TOOLCHAIN_TESTS_COMMANDS"
     . $HELPERS_DIR/populate-template-and-hash.sh -h # obtain $FULL_TAG (and don't overwrite existing file)
-    echo "$ docker run $ARGS $evars $FULL_TAG bash -c \"$TOOLCHAIN_TESTS_COMMANDS\""
-    eval docker run $ARGS $evars $FULL_TAG bash -c \"$TOOLCHAIN_TESTS_COMMANDS\"
+    echo "$ docker run $ARGS $FULL_TAG bash -c \"$TOOLCHAIN_TESTS_COMMANDS\""
+    eval docker run $ARGS $FULL_TAG bash -c \"$TOOLCHAIN_TESTS_COMMANDS\"
 
 fi
