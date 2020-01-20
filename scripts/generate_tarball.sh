haderech/eoscc

Add `eosio-ar` support in toolchain cmake to build static library

scripts/generate_tarball.sh
@@ -55,6 +55,7 @@ create_symlink "eosio-init eosio-init"
 create_symlink "eosio-abigen eosio-abigen"
 create_symlink "eosio-wasm2wast eosio-wasm2wast"
 create_symlink "eosio-wast2wasm eosio-wast2wasm"
+create_symlink "eoiso-ar eosio-ar"
 
 tar -cvzf $NAME ./${PREFIX}/*
 rm -r ${PREFIX}

EOSIO/eosio.cdt

scripts/generate_tarball.sh
@@ -55,6 +55,7 @@ create_symlink "eosio-init eosio-init"
 create_symlink "eosio-abigen eosio-abigen"
 create_symlink "eosio-wasm2wast eosio-wasm2wast"
 create_symlink "eosio-wast2wasm eosio-wast2wasm"
+create_symlink "eosio-ar eosio-ar"
 
 tar -cvzf $NAME ./${PREFIX}/*
 rm -r ${PREFIX}