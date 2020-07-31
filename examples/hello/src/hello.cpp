103667a396f78ba83af0eed4e394b39bceae83d1
iamveritas <ovidiu.craciun@block.one>
2020-07-31 18:44:53 +0300

    add example for returning values from action -- take 1

--- examples/hello/src/hello.cpp
+++ examples/hello/src/hello.cpp
@@ -1,4 +1,6 @@
+#include <eosiolib/capi/eosio/action.h>
 #include <hello.hpp>
+
 [[eosio::action]]
 void hello::hi( name nm ) {
    print_f("Name : %\n", nm);
@@ -8,4 +10,22 @@ void hello::hi( name nm ) {
 void hello::check( name nm ) {
    print_f("Name : %\n", nm);
    eosio::check(nm == "hello"_n, "check name not equal to `hello`");
+}
+
+[[eosio::action]]
+void hello::checkwithrv( name nm ) {
+   print_f("Name : %\n", nm);
+
+   std::vector<std::string> results;
+   if (nm == "hello"_n) {
+      results.push_back("0");
+      results.push_back("validation has passed");
+   }
+   else {
+      results.push_back("1");
+      results.push_back("name not equal to `hello`");
+   }
+
+   const auto& packed_results = eosio::pack(results);
+   set_action_return_value((void*)packed_results.data(), packed_results.size());
 }
