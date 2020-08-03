3358f762a7dc3ab0f0f81563309f85b3f819666c
iamveritas <ovidiu.craciun@block.one>
2020-08-03 15:44:46 +0300

    implement return values using set_action_return_value intrinsic

--- examples/hello/include/hello.hpp
+++ examples/hello/include/hello.hpp
@@ -11,8 +11,11 @@ class [[eosio::contract]] hello : public contract {
       void check( name nm );
       [[eosio::action]]
       std::vector<std::string> checkwithrv( name nm );
+      [[eosio::action]]
+      void checkwithrv2( name nm );
 
       using hi_action = action_wrapper<"hi"_n, &hello::hi>;
       using check_action = action_wrapper<"check"_n, &hello::check>;
       using checkwithrv_action = action_wrapper<"checkwithrv"_n, &hello::checkwithrv>;
+      using checkwithrv2_action = action_wrapper<"checkwithrv2"_n, &hello::checkwithrv2>;
 };
--- examples/hello/src/hello.cpp
+++ examples/hello/src/hello.cpp
@@ -24,6 +24,23 @@ std::vector<std::string> hello::checkwithrv( name nm ) {
       results.push_back("1");
       results.push_back("name not equal to `hello`");
    }
-
    return results;
+}
+
+[[eosio::action]]
+void hello::checkwithrv2( name nm ) {
+   print_f("Name : %\n", nm);
+
+   std::vector<std::string> results;
+   if (nm == "hello"_n) {
+      results.push_back("0");
+      results.push_back("validation has passed");
+   }
+   else {
+      results.push_back("1");
+      results.push_back("name not equal to `hello`");
+   }
+
+   const auto& packed_results = eosio::pack(results);
+   set_action_return_value((void*)packed_results.data(), packed_results.size());
 }
