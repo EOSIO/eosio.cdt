2c18dded9eea0db1c54dfb3e6119a52d9121ce95
Jeffrey Smith II <jeffreyssmith2nd@gmail.com>
2020-08-11 15:10:55 -0400

    Don't use reserved name

--- tests/toolchain/build-pass/bool_template.cpp
+++ tests/toolchain/build-pass/bool_template.cpp
@@ -2,7 +2,7 @@
 
 using namespace eosio;
 
-namespace _detail{
+namespace test {
    using _Bool = int32_t;
 }
 
@@ -16,6 +16,6 @@ class [[eosio::contract]] bool_template : public eosio::contract {
       [[eosio::action]] void test2(std::variant<uint64_t, bool> a) {}
       [[eosio::action]] void test3(bool a) {}
 
-      [[eosio::action]] void test4(_detail::_Bool a) {}
+      [[eosio::action]] void test4(test::_Bool a) {}
       [[eosio::action]] void test5(My_Bool a) {}
 };
