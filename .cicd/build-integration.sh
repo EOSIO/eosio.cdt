97f0c4564d450eed44c0790fc4bd4c462d619ba5
Jeffrey Smith II <jeffreyssmith2nd@gmail.com>
2020-09-03 14:16:53 -0400

    wip - change image handling

--- .cicd/build-integration.sh
+++ .cicd/build-integration.sh
@@ -8,17 +8,11 @@ ARGS=${ARGS:-"--rm --init -v $(pwd):$MOUNTED_DIR"}
 
 EOSIO_PATH="/root/eosio/build"
 
-# . $HELPERS_DIR/docker-hash.sh
-
 # PRE_COMMANDS: Executed pre-cmake
 PRE_COMMANDS="cd $MOUNTED_DIR/build"
 BUILD_COMMANDS="cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=${EOSIO_PATH} -DEOSIO_RUN_INTEGRATION_TESTS=1 .. && make -j$JOBS"
 
 # Docker Commands
-if [[ $BUILDKITE == true ]]; then
-    # Generate Base Images
-    $CICD_DIR/generate-base-images.sh
-fi
 
 COMMANDS="$PRE_COMMANDS && $BUILD_COMMANDS"
 
