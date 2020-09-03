86ee4f1363ed45788d73a5017ea4038e11503f34
Jeffrey Smith II <jeffreyssmith2nd@gmail.com>
2020-09-03 09:14:51 -0400

    Update integration tests for latest develop of eos

--- tests/integration/kv_tests.cpp
+++ tests/integration/kv_tests.cpp
@@ -20,18 +20,18 @@ using mvo = fc::mutable_variant_object;
 #endif
 
 void setup(TESTER& tester, std::vector<uint8_t> wasm, std::vector<char> abi) {
-   tester.create_accounts( { N(kvtest) } );
+   tester.create_accounts( { "kvtest"_n } );
    tester.produce_block();
-   tester.set_code( N(kvtest), wasm );
-   tester.set_abi( N(kvtest), abi.data() );
+   tester.set_code( "kvtest"_n, wasm );
+   tester.set_abi( "kvtest"_n, abi.data() );
    tester.produce_blocks();
 
    tester.set_code(config::system_account_name, contracts::kv_bios_wasm());
    tester.set_abi(config::system_account_name, contracts::kv_bios_abi().data());
 
    auto data = mvo()("k", 1024)("v", 1024*1024)("i", 256);
-   tester.push_action(config::system_account_name, N(ramkvlimits), config::system_account_name, data);
-   tester.push_action(N(kvtest), N(setup), N(kvtest), {});
+   tester.push_action(config::system_account_name, "ramkvlimits"_n, config::system_account_name, data);
+   tester.push_action("kvtest"_n, "setup"_n, "kvtest"_n, {});
 
    tester.produce_blocks();
 }
@@ -41,9 +41,9 @@ BOOST_AUTO_TEST_SUITE(key_value_tests)
 BOOST_AUTO_TEST_CASE(single_tests_find) try {
    TESTER tester;
    setup(tester, contracts::kv_single_tests_wasm(), contracts::kv_single_tests_abi());
-   tester.push_action(N(kvtest), N(find), N(kvtest), {});
+   tester.push_action("kvtest"_n, "find"_n, "kvtest"_n, {});
 
-   BOOST_CHECK_EXCEPTION(tester.push_action(N(kvtest), N(finderror), N(kvtest), {}),
+   BOOST_CHECK_EXCEPTION(tester.push_action("kvtest"_n, "finderror"_n, "kvtest"_n, {}),
                          eosio_assert_message_exception,
                          eosio_assert_message_is("Cannot read end iterator"));
 } FC_LOG_AND_RETHROW()
@@ -51,9 +51,9 @@ BOOST_AUTO_TEST_CASE(single_tests_find) try {
 BOOST_AUTO_TEST_CASE(single_tests_get) try {
    TESTER tester;
    setup(tester, contracts::kv_single_tests_wasm(), contracts::kv_single_tests_abi());
-   tester.push_action(N(kvtest), N(get), N(kvtest), {});
+   tester.push_action("kvtest"_n, "get"_n, "kvtest"_n, {});
 
-   BOOST_CHECK_EXCEPTION(tester.push_action(N(kvtest), N(geterror), N(kvtest), {}),
+   BOOST_CHECK_EXCEPTION(tester.push_action("kvtest"_n, "geterror"_n, "kvtest"_n, {}),
                          eosio_assert_message_exception,
                          eosio_assert_message_is("Key not found in `[]`"));
 } FC_LOG_AND_RETHROW()
@@ -61,19 +61,19 @@ BOOST_AUTO_TEST_CASE(single_tests_get) try {
 BOOST_FIXTURE_TEST_CASE(single_tests_bounds, tester) try {
    TESTER tester;
    setup(tester, contracts::kv_single_tests_wasm(), contracts::kv_single_tests_abi());
-   tester.push_action(N(kvtest), N(bounds), N(kvtest), {});
+   tester.push_action("kvtest"_n, "bounds"_n, "kvtest"_n, {});
 } FC_LOG_AND_RETHROW()
 
 BOOST_FIXTURE_TEST_CASE(single_tests_iteration, tester) try {
    TESTER tester;
    setup(tester, contracts::kv_single_tests_wasm(), contracts::kv_single_tests_abi());
-   tester.push_action(N(kvtest), N(iteration), N(kvtest), {});
+   tester.push_action("kvtest"_n, "iteration"_n, "kvtest"_n, {});
 
-   BOOST_CHECK_EXCEPTION(tester.push_action(N(kvtest), N(itrerror1), N(kvtest), {}),
+   BOOST_CHECK_EXCEPTION(tester.push_action("kvtest"_n, "itrerror1"_n, "kvtest"_n, {}),
                          eosio_assert_message_exception,
                          eosio_assert_message_is("cannot increment end iterator"));
 
-   BOOST_CHECK_EXCEPTION(tester.push_action(N(kvtest), N(itrerror2), N(kvtest), {}),
+   BOOST_CHECK_EXCEPTION(tester.push_action("kvtest"_n, "itrerror2"_n, "kvtest"_n, {}),
                          eosio_assert_message_exception,
                          eosio_assert_message_is("decremented past the beginning"));
 } FC_LOG_AND_RETHROW()
@@ -81,13 +81,13 @@ BOOST_FIXTURE_TEST_CASE(single_tests_iteration, tester) try {
 BOOST_FIXTURE_TEST_CASE(single_tests_reverse_iteration, tester) try {
    TESTER tester;
    setup(tester, contracts::kv_single_tests_wasm(), contracts::kv_single_tests_abi());
-   tester.push_action(N(kvtest), N(riteration), N(kvtest), {});
+   tester.push_action("kvtest"_n, "riteration"_n, "kvtest"_n, {});
 
-   BOOST_CHECK_EXCEPTION(tester.push_action(N(kvtest), N(ritrerror1), N(kvtest), {}),
+   BOOST_CHECK_EXCEPTION(tester.push_action("kvtest"_n, "ritrerror1"_n, "kvtest"_n, {}),
                          eosio_assert_message_exception,
                          eosio_assert_message_is("incremented past the end"));
 
-   BOOST_CHECK_EXCEPTION(tester.push_action(N(kvtest), N(ritrerror2), N(kvtest), {}),
+   BOOST_CHECK_EXCEPTION(tester.push_action("kvtest"_n, "ritrerror2"_n, "kvtest"_n, {}),
                          eosio_assert_message_exception,
                          eosio_assert_message_is("decremented past the beginning"));
 } FC_LOG_AND_RETHROW()
@@ -95,13 +95,13 @@ BOOST_FIXTURE_TEST_CASE(single_tests_reverse_iteration, tester) try {
 BOOST_FIXTURE_TEST_CASE(single_tests_range, tester) try {
    TESTER tester;
    setup(tester, contracts::kv_single_tests_wasm(), contracts::kv_single_tests_abi());
-   tester.push_action(N(kvtest), N(range), N(kvtest), {});
+   tester.push_action("kvtest"_n, "range"_n, "kvtest"_n, {});
 } FC_LOG_AND_RETHROW()
 
 BOOST_FIXTURE_TEST_CASE(single_tests_erase, tester) try {
    TESTER tester;
    setup(tester, contracts::kv_single_tests_wasm(), contracts::kv_single_tests_abi());
-   tester.push_action(N(kvtest), N(erase), N(kvtest), {});
+   tester.push_action("kvtest"_n, "erase"_n, "kvtest"_n, {});
 } FC_LOG_AND_RETHROW()
 
 // Multi
@@ -109,30 +109,30 @@ BOOST_FIXTURE_TEST_CASE(single_tests_erase, tester) try {
 BOOST_FIXTURE_TEST_CASE(multi_tests_get, tester) try {
    TESTER tester;
    setup(tester, contracts::kv_multi_tests_wasm(), contracts::kv_multi_tests_abi());
-   tester.push_action(N(kvtest), N(get), N(kvtest), {});
+   tester.push_action("kvtest"_n, "get"_n, "kvtest"_n, {});
 } FC_LOG_AND_RETHROW()
 
 BOOST_FIXTURE_TEST_CASE(multi_tests_iteration, tester) try {
    TESTER tester;
    setup(tester, contracts::kv_multi_tests_wasm(), contracts::kv_multi_tests_abi());
-   tester.push_action(N(kvtest), N(iteration), N(kvtest), {});
+   tester.push_action("kvtest"_n, "iteration"_n, "kvtest"_n, {});
 } FC_LOG_AND_RETHROW()
 
 BOOST_FIXTURE_TEST_CASE(multi_tests_non_unique, tester) try {
    TESTER tester;
    setup(tester, contracts::kv_multi_tests_wasm(), contracts::kv_multi_tests_abi());
-   tester.push_action(N(kvtest), N(nonunique), N(kvtest), {});
+   tester.push_action("kvtest"_n, "nonunique"_n, "kvtest"_n, {});
 } FC_LOG_AND_RETHROW()
 
 BOOST_FIXTURE_TEST_CASE(multi_tests_update, tester) try {
    TESTER tester;
    setup(tester, contracts::kv_multi_tests_wasm(), contracts::kv_multi_tests_abi());
-   tester.push_action(N(kvtest), N(update), N(kvtest), {});
+   tester.push_action("kvtest"_n, "update"_n, "kvtest"_n, {});
 
-   BOOST_CHECK_EXCEPTION(tester.push_action(N(kvtest), N(updateerr1), N(kvtest), {}),
+   BOOST_CHECK_EXCEPTION(tester.push_action("kvtest"_n, "updateerr1"_n, "kvtest"_n, {}),
                          eosio_assert_message_exception,
                          eosio_assert_message_is("Attempted to update an existing secondary index."));
-   BOOST_CHECK_EXCEPTION(tester.push_action(N(kvtest), N(updateerr2), N(kvtest), {}),
+   BOOST_CHECK_EXCEPTION(tester.push_action("kvtest"_n, "updateerr2"_n, "kvtest"_n, {}),
                          eosio_assert_message_exception,
                          eosio_assert_message_is("Attempted to store an existing secondary index."));
 } FC_LOG_AND_RETHROW()
@@ -142,26 +142,26 @@ BOOST_FIXTURE_TEST_CASE(multi_tests_update, tester) try {
 BOOST_FIXTURE_TEST_CASE(variant_tests, tester) try {
    TESTER tester;
    setup(tester, contracts::kv_variant_tests_wasm(), contracts::kv_variant_tests_abi());
-   tester.push_action(N(kvtest), N(vriant), N(kvtest), {});
+   tester.push_action("kvtest"_n, "vriant"_n, "kvtest"_n, {});
 } FC_LOG_AND_RETHROW()
 
 BOOST_FIXTURE_TEST_CASE(variant_upgrade_tests, tester) try {
    TESTER tester;
    setup(tester, contracts::kv_variant_tests_wasm(), contracts::kv_variant_tests_abi());
-   tester.push_action(N(kvtest), N(vriantupgrd), N(kvtest), {});
+   tester.push_action("kvtest"_n, "vriantupgrd"_n, "kvtest"_n, {});
 } FC_LOG_AND_RETHROW()
 
 // Make Key
 // --------
 std::vector<name> data_set{
-   N(makekeyname), N(makekeystr), N(makekeyuill), N(makekeyil), N(makekeyuilll),
-   N(makekeyflt), N(makekeydbl), N(makekeystct), N(makekeytup)
+   "makekeyname"_n, "makekeystr"_n, "makekeyuill"_n, "makekeyil"_n, "makekeyuilll"_n,
+   "makekeyflt"_n, "makekeydbl"_n, "makekeystct"_n, "makekeytup"_n
 };
 
 BOOST_DATA_TEST_CASE(makekeytests, data_set) { try {
    TESTER tester;
    setup(tester, contracts::kv_make_key_tests_wasm(), contracts::kv_make_key_tests_abi());
-   tester.push_action(N(kvtest), sample, N(kvtest), {});
+   tester.push_action("kvtest"_n, sample, "kvtest"_n, {});
 } FC_LOG_AND_RETHROW() }
 
 BOOST_AUTO_TEST_SUITE_END()
