cb945d1fd5205474d27a3188280ec65d5a7d66a9
Zach Butler <zach.butler@block.one>
2020-11-18 14:08:43 -0500

    Unified pipeline look-and-feel

--- .cicd/legacy-docker-build.sh
+++ .cicd/legacy-docker-build.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 set -eo pipefail
-echo "--- :arrow_down: Downloading package"
+echo '--- :arrow_down: Downloading Package :package:'
 buildkite-agent artifact download '*.deb' . --step ':ubuntu: Ubuntu 18.04 - Package Builder'
-echo "--- :key: AUTHENTICATING GOOGLE SERVICE ACCOUNT"
+echo '--- :key: Authenticating Google Service Account :gcloud:'
 gcloud --quiet auth activate-service-account b1-automation-svc@b1-automation-dev.iam.gserviceaccount.com --key-file=/etc/gcp-service-account.json
 docker-credential-gcr configure-docker
-echo "--- :hammer_and_wrench: BUILDING BUILD IMAGE"
-[[ "$BUILDKITE_TAG" != "" ]] && docker build -f .cicd/legacy.dockerfile -t eosio/cdt:latest -t eosio/cdt:$BUILDKITE_COMMIT -t eosio/cdt:$BUILDKITE_BRANCH -t eosio/cdt:$BUILDKITE_TAG . || docker build -f .cicd/legacy.dockerfile -t eosio/cdt:latest -t eosio/cdt:$BUILDKITE_COMMIT -t eosio/cdt:$BUILDKITE_BRANCH .
+echo '+++ :docker: Building Image'
+[[ "$BUILDKITE_TAG" != '' ]] && docker build -f .cicd/legacy.dockerfile -t eosio/cdt:latest -t eosio/cdt:$BUILDKITE_COMMIT -t eosio/cdt:$BUILDKITE_BRANCH -t eosio/cdt:$BUILDKITE_TAG . || docker build -f .cicd/legacy.dockerfile -t eosio/cdt:latest -t eosio/cdt:$BUILDKITE_COMMIT -t eosio/cdt:$BUILDKITE_BRANCH .
 docker tag eosio/cdt:$BUILDKITE_COMMIT gcr.io/b1-automation-dev/eosio/cdt:$BUILDKITE_COMMIT
 docker tag eosio/cdt:$BUILDKITE_BRANCH gcr.io/b1-automation-dev/eosio/cdt:$BUILDKITE_BRANCH
-[[ "$BUILDKITE_TAG" != "" ]] && docker tag eosio/cdt:$BUILDKITE_TAG gcr.io/b1-automation-dev/eosio/cdt:$BUILDKITE_TAG || :
+[[ "$BUILDKITE_TAG" != '' ]] && docker tag eosio/cdt:$BUILDKITE_TAG gcr.io/b1-automation-dev/eosio/cdt:$BUILDKITE_TAG || :
 docker tag eosio/cdt:latest gcr.io/b1-automation-dev/eosio/cdt:latest
-echo "--- :hand: PUSHING DOCKER IMAGES"
+echo '+++ :arrow_up: Pushing Docker Images'
 docker push gcr.io/b1-automation-dev/eosio/cdt:$BUILDKITE_COMMIT
 docker push gcr.io/b1-automation-dev/eosio/cdt:$BUILDKITE_BRANCH
-[[ "$BUILDKITE_TAG" != "" ]] && docker push gcr.io/b1-automation-dev/eosio/cdt:$BUILDKITE_TAG || :
+[[ "$BUILDKITE_TAG" != '' ]] && docker push gcr.io/b1-automation-dev/eosio/cdt:$BUILDKITE_TAG || :
 docker push gcr.io/b1-automation-dev/eosio/cdt:latest
-echo "--- :thought_balloon: TRASHING OLD IMAGES"
+echo '+++ :put_litter_in_its_place: Cleaning Up'
 docker rmi eosio/cdt:$BUILDKITE_COMMIT
 docker rmi eosio/cdt:$BUILDKITE_BRANCH
-[[ "$BUILDKITE_TAG" != "" ]] && docker rmi eosio/cdt:$BUILDKITE_TAG || :
+[[ "$BUILDKITE_TAG" != '' ]] && docker rmi eosio/cdt:$BUILDKITE_TAG || :
 docker rmi eosio/cdt:latest
 docker rmi gcr.io/b1-automation-dev/eosio/cdt:$BUILDKITE_COMMIT
 docker rmi gcr.io/b1-automation-dev/eosio/cdt:$BUILDKITE_BRANCH
-[[ "$BUILDKITE_TAG" != "" ]] && docker rmi gcr.io/b1-automation-dev/eosio/cdt:$BUILDKITE_TAG || :
+[[ "$BUILDKITE_TAG" != '' ]] && docker rmi gcr.io/b1-automation-dev/eosio/cdt:$BUILDKITE_TAG || :
 docker rmi gcr.io/b1-automation-dev/eosio/cdt:latest
