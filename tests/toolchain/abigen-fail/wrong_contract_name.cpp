fbaed1807311709cbe6075b605f62f28bca15040
Qing Yang <qing.yang@block.one>
2021-01-29 10:38:50 -0500

    simplify the contracts for abigen-fail testing

--- tests/toolchain/abigen-fail/wrong_contract_name.cpp
+++ tests/toolchain/abigen-fail/wrong_contract_name.cpp
@@ -5,18 +5,7 @@ CONTRACT hello : public contract {
    public:
       using contract::contract;
 
-      ACTION hi( name nm );
-      ACTION check( name nm );
-
-      using hi_action = action_wrapper<"hi"_n, &hello::hi>;
-      using check_action = action_wrapper<"check"_n, &hello::check>;
+      ACTION hi( name nm ) {
+         print_f("Name : %\n", nm);
+      }
 };
-
-ACTION hello::hi( name nm ) {
-   print_f("Name : %\n", nm);
-}
-
-ACTION hello::check( name nm ) {
-   print_f("Name : %\n", nm);
-   eosio::check(nm == "hello"_n, "check name not equal to `hello`");
-}
