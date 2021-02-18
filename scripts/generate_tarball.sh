591fa9a04e54e92f858453d869048b294f2d4868
Venu Kailasa <venu.kailasa-ext@block.one>
2021-02-18 15:38:24 -0600

    Adding clang 9 libc++abi.so to ubuntu 16.04 package

--- scripts/generate_tarball.sh
+++ scripts/generate_tarball.sh
@@ -38,7 +38,10 @@ cp ${BUILD_DIR}/lib/*.a ${CDT_PREFIX}/lib || exit 1
 # install libc++.so
 if [[ "$OS" == "ubuntu-16.04" ]]; then
     cp /usr/lib/libc++.so.1.0 ${CDT_PREFIX}/lib || exit 1
-    ln -sf ${CDT_PREFIX}/lib/libc++.so.1.0 ${CDT_PREFIX}/lib/libc++.so.1
+    cp /usr/lib/libc++abi.so.1.0 ${CDT_PREFIX}/lib || exit 1
+    cd ${CDT_PREFIX}/lib || exit 1
+    ln -sf libc++.so.1.0 libc++.so.1 || exit 1
+    ln -sf libc++abi.so.1.0 libc++abi.so.1 || exit 1
 fi
 
 # make symlinks
