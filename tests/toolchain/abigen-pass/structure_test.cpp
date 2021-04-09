872d8ccced5679160df3faf3429d0004aa6d6b1a
Venu Kailasa <venu.kailasa-ext@block.one>
2021-04-09 13:13:59 -0500

    Adding unit test for eosio::structure attribute

---
+++ tests/toolchain/abigen-pass/structure_test.cpp
@@ -0,0 +1,18 @@
+#include <eosio/eosio.hpp>
+
+using namespace eosio;
+
+struct [[eosio::structure]] fee_payer {
+   name payer;
+   uint64_t quantity;
+};
+
+class [[eosio::contract]] structure_test : public eosio::contract {
+public:
+   using contract::contract;
+
+   [[eosio::action]]
+   void printfeepyr(fee_payer pyr) {
+      print(pyr.payer, pyr.quantity);
+   }
+};
