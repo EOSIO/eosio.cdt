9bae28bae66a6688343626831f5f2cca272a1772
Venu Kailasa <venu.kailasa-ext@block.one>
2021-04-15 08:58:04 -0500

    Adding another action to structure test, and deleting wasm file

--- tests/toolchain/abigen-pass/structure_test.cpp
+++ tests/toolchain/abigen-pass/structure_test.cpp
@@ -2,17 +2,26 @@
 
 using namespace eosio;
 
-struct [[eosio::structure]] fee_payer {
-   name payer;
-   uint64_t quantity;
-};
-
 class [[eosio::contract]] structure_test : public eosio::contract {
 public:
    using contract::contract;
 
+   struct [[eosio::structure]] fee_payer {
+      name payer;
+      uint64_t quantity;
+   };
+
    [[eosio::action]]
-   void setfeepyr(fee_payer pyr) {
+   fee_payer getpayer() {
+      fee_payer pyr;
+      pyr.payer = _self;
+      pyr.quantity = 1000;
+
+      return pyr;
+   }
+
+   [[eosio::action]]
+   void setfeepyr(fee_payer& pyr) {
       pyr.payer = "respyr"_n;
       pyr.quantity = 10;
    }
