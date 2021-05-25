b2bd2fff2cc115988ceafd6ad9d713c689693b2f
Qing Yang <qing.yang@block.one>
2021-05-25 11:52:05 -0400

    add rt tests to cmake

---
+++ libraries/rt/test/rt_tests.cmake
@@ -0,0 +1,9 @@
+execute_process(COMMAND test_ashlti3
+                COMMAND test_ashrti3
+                COMMAND test_lshrti3
+                COMMAND test_divti3
+                COMMAND test_udivti3
+                COMMAND test_multi3
+                COMMAND test_modti3
+                COMMAND test_umodti3
+)
