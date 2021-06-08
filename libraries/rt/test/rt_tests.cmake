eeceb671e46631c9c7ec53773123dd4435d4c673
Victor Camacho <victor.camacho@block.one>
2021-06-08 10:48:11 -0400

    CMake change - also run ctest - rt_tests from top_directory

--- libraries/rt/test/rt_tests.cmake
+++ libraries/rt/test/rt_tests.cmake
@@ -1,9 +1,10 @@
-execute_process(COMMAND test_ashlti3
-                COMMAND test_ashrti3
-                COMMAND test_lshrti3
-                COMMAND test_divti3
-                COMMAND test_udivti3
-                COMMAND test_multi3
-                COMMAND test_modti3
-                COMMAND test_umodti3
+execute_process(COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/test_ashlti3
+                COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/test_ashrti3
+                COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/test_lshrti3
+                COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/test_divti3
+                COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/test_udivti3
+                COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/test_multi3
+                COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/test_modti3
+                COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/test_umodti3
+                COMMAND_ERROR_IS_FATAL ANY
 )
