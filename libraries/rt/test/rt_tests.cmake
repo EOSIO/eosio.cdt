616d31a8637f2a9b48479d72859cab82558e173b
Qing Yang <qing.yang@block.one>
2021-06-08 10:38:21 -0400

    fix return code checking for tests

--- libraries/rt/test/rt_tests.cmake
+++ libraries/rt/test/rt_tests.cmake
@@ -1,9 +1,39 @@
-execute_process(COMMAND test_ashlti3
-                COMMAND test_ashrti3
-                COMMAND test_lshrti3
-                COMMAND test_divti3
-                COMMAND test_udivti3
-                COMMAND test_multi3
-                COMMAND test_modti3
-                COMMAND test_umodti3
-)
+execute_process(COMMAND ${CMAKE_CURRENT_BINARY_DIR}/test_ashlti3 RESULT_VARIABLE res)
+if(res EQUAL "1")
+    message( FATAL_ERROR "test_ashlti3 failed")
+endif()
+
+execute_process(COMMAND ${CMAKE_CURRENT_BINARY_DIR}/test_ashrti3 RESULT_VARIABLE res)
+if(res EQUAL "1")
+    message( FATAL_ERROR "test_ashrti3 failed")
+endif()
+
+execute_process(COMMAND ${CMAKE_CURRENT_BINARY_DIR}/test_lshrti3 RESULT_VARIABLE res)
+if(res EQUAL "1")
+    message( FATAL_ERROR "test_lshrti3 failed")
+endif()
+
+execute_process(COMMAND ${CMAKE_CURRENT_BINARY_DIR}/test_divti3 RESULT_VARIABLE res)
+if(res EQUAL "1")
+    message( FATAL_ERROR "test_divti3 failed")
+endif()
+
+execute_process(COMMAND ${CMAKE_CURRENT_BINARY_DIR}/test_udivti3 RESULT_VARIABLE res)
+if(res EQUAL "1")
+    message( FATAL_ERROR "test_udivti3 failed")
+endif()
+
+execute_process(COMMAND ${CMAKE_CURRENT_BINARY_DIR}/test_multi3 RESULT_VARIABLE res)
+if(res EQUAL "1")
+    message( FATAL_ERROR "test_multi3 failed")
+endif()
+
+execute_process(COMMAND ${CMAKE_CURRENT_BINARY_DIR}/test_modti3 RESULT_VARIABLE res)
+if(res EQUAL "1")
+    message( FATAL_ERROR "test_modti3 failed")
+endif()
+
+execute_process(COMMAND ${CMAKE_CURRENT_BINARY_DIR}/test_umodti3 RESULT_VARIABLE res)
+if(res EQUAL "1")
+    message( FATAL_ERROR "test_umodti3 failed")
+endif()
