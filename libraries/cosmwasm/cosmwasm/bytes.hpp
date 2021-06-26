80c35ecf410461cca614500f351cae75d35bf522
Jeeyong Um <conr2d@gmail.com>
2021-06-26 11:11:33 +0000

    cosmwasm: Fix bytes encoding error

--- libraries/cosmwasm/cosmwasm/bytes.hpp
+++ libraries/cosmwasm/cosmwasm/bytes.hpp
@@ -2,15 +2,13 @@
 #include "json.hpp"
 
 namespace cosmwasm {
-
    template<typename T>
-   T from_bytes(const bytes& b) {
-      std::string_view s(b.data(), b.size());
+   T from_bytes(const bytes& b) { std::string_view s(b.data(), b.size());
       json::value v = json::parse_unsafe(s);
       return json::from_json<T>(v);
    }
 
-   template<typename T>
+   template<typename T, std::enable_if_t<!std::is_integral_v<std::decay_t<T>>, int> = 0>
    bytes to_bytes(const T& v) {
       return v.as_bytes();
    }
@@ -29,17 +27,20 @@ namespace cosmwasm {
 
    template<typename T, std::enable_if_t<std::is_integral_v<std::decay_t<T>>, int> = 0>
    bytes to_bytes(T v) {
-      union {
-         T u;
-         unsigned char u8[sizeof(T)];
-      } source, dest;
+      auto s = std::to_string(v);
+      return to_bytes(s);
+   }
 
-      source.u = v;
+   template<>
+   bytes to_bytes(uint128_t v) {
+      auto s = "\"" + std::to_string(v) + "\"";
+      return to_bytes(s);
+   }
 
-      for (size_t k = 0; k < sizeof(T); ++k) {
-         dest.u8[k] = source.u8[sizeof(T) - k - 1];
-      }
-
-      return {dest.u8};
+   template<size_t N>
+   bytes to_bytes(char const (&v)[N]) {
+      bytes out(N-1);
+      std::copy_n(std::begin(v), N-1, out.begin());
+      return out;
    }
 }
