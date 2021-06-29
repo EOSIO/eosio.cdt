62990d3aacb1bd11e9d0ad615fe63936788c961c
Scott Arnette <scott.arnette@block.one>
2021-06-29 15:02:45 -0400

    More accurate variable naming.

--- .cicd/generate-pipeline.sh
+++ .cicd/generate-pipeline.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -eou pipefail
 
-export MACOS_10_14_FILE_HASH="eosio-cdt-macos-10.14-$(sha1sum ./.cicd/docker/macos-10.14.sh | awk '{print $1}')"
-export MACOS_10_15_FILE_HASH="eosio-cdt-macos-10.15-$(sha1sum ./.cicd/docker/macos-10.15.sh | awk '{print $1}')"
-export VARS='$MACOS_10_14_FILE_HASH:$MACOS_10_15_FILE_HASH'
+export MACOS_10_14_TAG="eosio-cdt-macos-10.14-$(sha1sum ./.cicd/docker/macos-10.14.sh | awk '{print $1}')"
+export MACOS_10_15_TAG="eosio-cdt-macos-10.15-$(sha1sum ./.cicd/docker/macos-10.15.sh | awk '{print $1}')"
+export VARS='$MACOS_10_14_TAG:$MACOS_10_15_TAG'
 envsubst "$VARS" < "./.cicd/pipeline.yml" > "./.cicd/pipeline.yml.out"
 buildkite-agent artifact upload ./.cicd/pipeline.yml.out
 buildkite-agent pipeline upload ./.cicd/pipeline.yml.out
