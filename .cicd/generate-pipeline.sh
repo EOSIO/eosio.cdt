6c5c7e3d97daf5ec7c78c2e135de900a4ad14c20
Scott Arnette <scott.arnette@block.one>
2021-06-29 13:52:24 -0400

    Fix path.

--- .cicd/generate-pipeline.sh
+++ .cicd/generate-pipeline.sh
@@ -5,5 +5,5 @@ export MACOS_10_14_FILE_HASH="eosio-cdt-macos-10.14-$(sha1sum ./.cicd/docker/mac
 export MACOS_10_15_FILE_HASH="eosio-cdt-macos-10.15-$(sha1sum ./.cicd/docker/macos-10.15.sh | awk '{print $1}')"
 export VARS='$MACOS_10_14_FILE_HASH:$MACOS_10_15_FILE_HASH'
 envsubst "$VARS" < "./.cicd/pipeline.yml" > "./.cicd/pipeline.yml.out"
-buildkite-agent artifact upload pipeline.yml.out
-buildkite-agent pipeline upload pipeline.yml.out
+buildkite-agent artifact upload ./.cicd/pipeline.yml.out
+buildkite-agent pipeline upload ./.cicd/pipeline.yml.out
