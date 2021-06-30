479142ed0a294b8e1c8260e849563434bb30b953
William Blevins <william.blevins@block.one>
2021-06-30 15:48:44 -0400

    Remove extra macos version references.

--- .cicd/pipeline-upload.sh
+++ .cicd/pipeline-upload.sh
@@ -2,8 +2,7 @@
 set -eou pipefail
 
 export MACOS_10_14_TAG="eosio-cdt-macos-10.14-$(sha1sum ./.cicd/platforms/macos-10.14.sh | awk '{print $1}')"
-export MACOS_10_15_TAG="eosio-cdt-macos-10.15-$(sha1sum ./.cicd/platforms/macos-10.15.sh | awk '{print $1}')"
-export VARS='$MACOS_10_14_TAG:$MACOS_10_15_TAG'
+export VARS='$MACOS_10_14_TAG'
 envsubst "$VARS" < "./.cicd/pipeline.yml" > "./.cicd/pipeline.yml.out"
 buildkite-agent artifact upload ./.cicd/pipeline.yml.out
 buildkite-agent pipeline upload ./.cicd/pipeline.yml.out
