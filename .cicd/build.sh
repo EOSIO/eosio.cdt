5af0828b4606c23166d651bdb615b5feacf79d3e
Farhad Shahabi <farhad.shahabi@colorado.edu>
2021-07-27 16:44:38 -0400

    Update build.sh

--- .cicd/build.sh
+++ .cicd/build.sh
@@ -16,10 +16,10 @@ if [[ $(uname) == 'Darwin' ]]; then
     cmake .. -DCMAKE_BUILD_TYPE=Release
     make -j$JOBS
     export PATH=$BIN_DIR_PATH:$PATH
-    cd $CONTRACTS_DIR_PATH
-    mkdir -p build
-    cd build
-    cmake .. -DCMAKE_BUILD_TYPE=Release
+    cd $BUILD_DIR_PATH/tests/unit/test_contracts
+    mkdir -p eosio.contracts
+    cd eosio.contracts
+    cmake $CONTRACTS_DIR_PATH -DCMAKE_BUILD_TYPE=Release
     make -j$JOBS
 
 else # Linux
@@ -44,8 +44,8 @@ else # Linux
         fi
     fi
 
-    PRE_CONTRACTS_COMMAND="export PATH=$MOUNTED_DIR/build/bin:$PATH && cd $MOUNTED_DIR/eosio.contracts && mkdir -p build && cd build"
-    BUILD_CONTRACTS_COMMAND="cmake .. && make -j$JOBS"
+    PRE_CONTRACTS_COMMAND="export PATH=$MOUNTED_DIR/build/bin:$PATH && cd $MOUNTED_DIR/build/tests/unit/test_contracts && mkdir -p eosio.contracts && cd eosio.contracts"
+    BUILD_CONTRACTS_COMMAND="cmake $MOUNTED_DIR/eosio.contracts && make -j$JOBS"
 
     COMMANDS="$PRE_COMMANDS && $BUILD_COMMANDS && $PRE_CONTRACTS_COMMAND && $BUILD_CONTRACTS_COMMAND"
 
@@ -63,31 +63,48 @@ fi
 
 if [[ $BUILDKITE == true ]]; then
     cd $BUILD_DIR_PATH
-    touch wasm_size.log
+    touch wasm_abi_size.log
     PATH_WASM=$(pwd)
     cd tests/unit/test_contracts
-    echo '--- :arrow_up: Generating wasm_size.log file'
-    echo '####### EOSIO test contracts wasm files sizes #######' >> $PATH_WASM/wasm_size.log
+    echo '--- :arrow_up: Generating wasm_abi_size.log file'
+    echo '####### EOSIO test contracts wasm files sizes #######' >> $PATH_WASM/wasm_abi_size.log
     for FILENAME in ./*.wasm; do
         FILESIZE=$(wc -c "$FILENAME")
-        echo $FILESIZE >> $PATH_WASM/wasm_size.log
+        echo $FILESIZE >> $PATH_WASM/wasm_abi_size.log
+    done
+    echo '####### EOSIO test contracts abi files sizes #######' >> $PATH_WASM/wasm_abi_size.log
+    for FILENAME in ./*.abi; do
+        FILESIZE=$(wc -c "$FILENAME")
+        echo $FILESIZE >> $PATH_WASM/wasm_abi_size.log
     done
 
-    echo '####### EOSIO system contracts wasm files sizes #######' >> $PATH_WASM/wasm_size.log
-    cd $CONTRACTS_DIR_PATH/build/contracts
+    echo '####### EOSIO system contracts wasm files sizes #######' >> $PATH_WASM/wasm_abi_size.log
+    cd $BUILD_DIR_PATH/tests/unit/test_contracts/eosio.contracts/contracts
     for dir in */; do
         cd $dir
         for FILENAME in ./*.wasm; do
             if [[ -f $FILENAME ]]; then
                 FILESIZE=$(wc -c "$FILENAME")
-                echo $FILESIZE >> $PATH_WASM/wasm_size.log
+                echo $FILESIZE >> $PATH_WASM/wasm_abi_size.log
             fi
         done
         cd ..
     done
 
-    echo '--- :arrow_up: Uploading wasm_size.log'
+    echo '####### EOSIO system contracts abi files sizes #######' >> $PATH_WASM/wasm_abi_size.log
+    for dir in */; do
+        cd $dir
+        for FILENAME in ./*.abi; do
+            if [[ -f $FILENAME ]]; then
+                FILESIZE=$(wc -c "$FILENAME")
+                echo $FILESIZE >> $PATH_WASM/wasm_abi_size.log
+            fi
+        done
+        cd ..
+    done
+
+    echo '--- :arrow_up: Uploading wasm_abi_size.log'
     cd $PATH_WASM
-    buildkite-agent artifact upload wasm_size.log
-    echo 'Done uploading wasm_size.log'
+    buildkite-agent artifact upload wasm_abi_size.log
+    echo 'Done uploading wasm_abi_size.log'
 fi
