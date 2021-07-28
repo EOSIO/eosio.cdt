aa18ec0632446754be3ce942d4dc8c4dbf4d01c3
Farhad Shahabi <farhad.shahabi@colorado.edu>
2021-07-28 15:56:44 -0400

    Update build.sh

--- .cicd/build.sh
+++ .cicd/build.sh
@@ -19,7 +19,7 @@ if [[ $(uname) == 'Darwin' ]]; then
     cd $BUILD_DIR_PATH/tests/unit/test_contracts
     mkdir -p eosio.contracts
     cd eosio.contracts
-    cmake -DBUILD_TESTS=true $CONTRACTS_DIR_PATH 
+    cmake $CONTRACTS_DIR_PATH 
     make -j$JOBS
 
 else # Linux
@@ -45,9 +45,10 @@ else # Linux
     fi
 
     PRE_CONTRACTS_COMMAND="export PATH=$MOUNTED_DIR/build/bin:$PATH && cd $MOUNTED_DIR/build/tests/unit/test_contracts && mkdir -p eosio.contracts && cd eosio.contracts"
-    BUILD_CONTRACTS_COMMAND="cmake -DBUILD_TESTS=true $MOUNTED_DIR/eosio.contracts && make -j$JOBS"
+    BUILD_CONTRACTS_COMMAND="cmake $MOUNTED_DIR/eosio.contracts && make -j$JOBS"
 
-    COMMANDS="$PRE_COMMANDS && $BUILD_COMMANDS && $PRE_CONTRACTS_COMMAND && $BUILD_CONTRACTS_COMMAND"
+    COMMANDS="$PRE_COMMANDS && $BUILD_COMMANDS"
+    COMMANDS_EOSIO_CONTRACTS="$PRE_CONTRACTS_COMMAND && $BUILD_CONTRACTS_COMMAND"
 
     # Load BUILDKITE Environment Variables for use in docker run
     if [[ -f $BUILDKITE_ENV_FILE ]]; then
@@ -58,28 +59,33 @@ else # Linux
     fi
 
     eval docker run $ARGS $evars $FULL_TAG bash -c \"$COMMANDS\"
+    START_TIME=$SECONDS
+    eval docker run $ARGS $evars $FULL_TAG bash -c \"$COMMANDS_EOSIO_CONTRACTS\"
+    ELAPSED_TIME=$(($SECONDS - $START_TIME))
 
 fi
 
 if [[ $BUILDKITE == true ]]; then
-    cd $BUILD_DIR_PATH
+    cd $BUILD_DIR_PATH/tests/unit/test_contracts
     touch wasm_abi_size.log
     PATH_WASM=$(pwd)
-    cd tests/unit/test_contracts
     echo '--- :arrow_up: Generating wasm_abi_size.log file'
-    echo '####### EOSIO test contracts wasm files sizes #######' >> $PATH_WASM/wasm_abi_size.log
+    echo '####### EOSIO test contracts wasm files sizes #######'>> $PATH_WASM/wasm_abi_size.log
+    echo '####### wasm files path: tests/unit/test_contracts #######' >> $PATH_WASM/wasm_abi_size.log
     for FILENAME in ./*.wasm; do
         FILESIZE=$(wc -c "$FILENAME")
         echo $FILESIZE >> $PATH_WASM/wasm_abi_size.log
     done
     echo '####### EOSIO test contracts abi files sizes #######' >> $PATH_WASM/wasm_abi_size.log
+    echo '####### abi files path: tests/unit/test_contracts #######' >> $PATH_WASM/wasm_abi_size.log
     for FILENAME in ./*.abi; do
         FILESIZE=$(wc -c "$FILENAME")
         echo $FILESIZE >> $PATH_WASM/wasm_abi_size.log
     done
 
     echo '####### EOSIO system contracts wasm files sizes #######' >> $PATH_WASM/wasm_abi_size.log
-    cd $BUILD_DIR_PATH/tests/unit/test_contracts/eosio.contracts/contracts
+    echo '####### wasm files path: tests/unit/test_contracts/eosio.contracts/contracts #######' >> $PATH_WASM/wasm_abi_size.log
+    cd eosio.contracts/contracts
     for dir in */; do
         cd $dir
         for FILENAME in ./*.wasm; do
@@ -92,6 +98,7 @@ if [[ $BUILDKITE == true ]]; then
     done
 
     echo '####### EOSIO system contracts abi files sizes #######' >> $PATH_WASM/wasm_abi_size.log
+    echo '####### abi files path: tests/unit/test_contracts/eosio.contracts/contracts #######' >> $PATH_WASM/wasm_abi_size.log
     for dir in */; do
         cd $dir
         for FILENAME in ./*.abi; do
@@ -102,6 +109,8 @@ if [[ $BUILDKITE == true ]]; then
         done
         cd ..
     done
+
+    echo "Elapsed time to build eosio.contracts: $ELAPSED_TIME" >> $PATH_WASM/wasm_abi_size.log
 
     echo '--- :arrow_up: Uploading wasm_abi_size.log'
     cd $PATH_WASM
