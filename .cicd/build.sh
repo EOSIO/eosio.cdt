3349fd617f01a611b998b89447a4667e073fde64
Farhad Shahabi <farhad.shahabi@colorado.edu>
2021-07-30 15:55:50 -0400

    Update build.sh

--- .cicd/build.sh
+++ .cicd/build.sh
@@ -7,7 +7,10 @@ CDT_DIR_PATH=$(pwd)
 BUILD_DIR_PATH=$CDT_DIR_PATH/$BUILD_DIR
 BIN_DIR_PATH=$BUILD_DIR_PATH/bin
 git clone https://github.com/EOSIO/eosio.contracts.git
+cd eosio.contracts
 git switch develop
+git pull
+cd ..
 CONTRACTS_DIR_PATH=$(pwd)/eosio.contracts
 
 if [[ $(uname) == 'Darwin' ]]; then
