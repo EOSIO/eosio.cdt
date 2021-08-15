1f5319b45a6064e5bcb3c5041bdb1482c5281d24
Farhad Shahabi <farhad.shahabi@colorado.edu>
2021-08-15 10:57:05 -0400

    Update wasm_size_test.sh

--- .cicd/wasm_size_test.sh
+++ .cicd/wasm_size_test.sh
@@ -50,7 +50,7 @@ else #Linux
 fi
 
 if [[ $BUILDKITE == true ]]; then
-    cd $CDT_DIR_HOST/tests/unit/test_contracts
+    cd $CDT_DIR_HOST/build/tests/unit/test_contracts
     touch wasm_abi_size.json
     PATH_WASM=$(pwd)
     JSON=$(echo '{}' | jq -r '.')
