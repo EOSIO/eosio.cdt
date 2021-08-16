3063c87c025237981887a1c6916da7ff44966a9a
Farhad Shahabi <farhad.shahabi@colorado.edu>
2021-08-16 11:07:02 -0400

    Update macos-10.15.sh

--- .cicd/platforms/macos-10.15.sh
+++ .cicd/platforms/macos-10.15.sh
@@ -3,4 +3,10 @@ set -eou pipefail
 VERSION=1
 
 brew update && brew upgrade
-brew install automake cmake doxygen gettext git gmp graphviz lcov libtool python@3 wget
+brew install automake cmake doxygen gettext git gmp graphviz lcov libtool python@3 wget coreutils gnu-sed grep tree
+
+echo 'export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"' >> ~/.bash_profile
+echo 'export PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"' >> ~/.bash_profile
+echo 'export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"' >> ~/.bash_profile
+echo '' >> ~/.bash_profile
+source ~/.bash_profile
