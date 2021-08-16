02fd25e5eac71c8a9e504f23a58c6e7d2b65995f
Farhad Shahabi <farhad.shahabi@colorado.edu>
2021-08-16 12:41:19 -0400

    Update macos-10.14.sh

--- .cicd/platforms/macos-10.14.sh
+++ .cicd/platforms/macos-10.14.sh
@@ -3,6 +3,7 @@ set -eou pipefail
 VERSION=1
 
 brew update && brew upgrade
+brew unlink md5sha1sum
 brew install coreutils gnu-sed grep tree automake cmake doxygen gettext git gmp graphviz lcov libtool python@3 wget 
 
 echo 'export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"' >> ~/.bash_profile
