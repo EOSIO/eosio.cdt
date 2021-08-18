6a2ea85048cb057434c1e1f3c24c0503c9d098df
Farhad Shahabi <farhad.shahabi@colorado.edu>
2021-08-18 17:19:21 -0400

    Update general.sh

--- .cicd/helpers/general.sh
+++ .cicd/helpers/general.sh
@@ -4,3 +4,12 @@ export CICD_DIR=$ROOT_DIR/.cicd
 export HELPERS_DIR=$CICD_DIR/helpers
 export JOBS=${JOBS:-"$(getconf _NPROCESSORS_ONLN)"}
 export MOUNTED_DIR='/workdir'
+
+[[ -z $PIPELINE_CONFIG ]] && export PIPELINE_CONFIG='pipeline.json'
+[[ -z $RAW_PIPELINE_CONFIG ]] && export RAW_PIPELINE_CONFIG='pipeline.jsonc'
+if [[ -f "$RAW_PIPELINE_CONFIG" ]]; then
+    # parse pipeline config
+    cat "$RAW_PIPELINE_CONFIG" | grep -Po '^[^"/]*("((?<=\\).|[^"])*"[^"/]*)*' | jq -c '.[env.BUILDKITE_PIPELINE_SLUG]' > "$PIPELINE_CONFIG"
+elif [[ "$DEBUG" == 'true' ]]; then
+    echo "WARNING: No pipeline configuration file \"$RAW_PIPELINE_CONFIG\" found in \"$(pwd)\""'!'
+fi
