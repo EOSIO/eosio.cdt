98570020815e327ccda67f3178eef2cadb4bc77a
Qing Yang <qing.yang@block.one>
2021-08-31 15:58:04 -0400

    remove previous partial fix

--- libraries/eosiolib/contract_name.cpp
+++
@@ -1,6 +0,0 @@
-#include <cstdint>
-
-extern "C" {
-    volatile uint64_t eosio_contract_name = 0;
-    volatile void eosio_set_contract_name(uint64_t n) { eosio_contract_name = n; } // LLVM creates the call to this at the beginning of apply
-}
--- libraries/eosiolib/eosiolib.cpp
+++ libraries/eosiolib/eosiolib.cpp
@@ -5,6 +5,9 @@
 
 #include <algorithm>
 
+extern "C" volatile uint64_t eosio_contract_name = 0;
+extern "C" volatile void eosio_set_contract_name(uint64_t n) { eosio_contract_name = n; } // LLVM creates the call to this at the beginning of apply
+
 namespace eosio {
    extern "C" {
       __attribute__((eosio_wasm_import))
