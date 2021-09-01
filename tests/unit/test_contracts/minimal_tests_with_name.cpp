bd8db52c7a874dd275c8d7e70aad45e241115aac
Qing Yang <qing.yang@block.one>
2021-09-01 17:20:37 -0400

    remove extra test contracts

--- tests/unit/test_contracts/minimal_tests_with_name.cpp
+++
@@ -1,9 +0,0 @@
-// Verifies that the dispatching code is self-contained
-#include <eosio/eosio.hpp>
-
-class [[eosio::contract]] minimal_tests_with_name {
- public:
-   template<typename N, typename DS>
-   explicit constexpr minimal_tests_with_name(const N&, const N&, const DS&) {}
-   [[eosio::action]] void test1(eosio::name memo) {}
-};
