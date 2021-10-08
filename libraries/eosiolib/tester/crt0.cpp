fd6c11aef1a3328e816aa2bfb615c278ebdcd0b9
Huang-Ming Huang <huangh@objectcomputing.com>
2021-10-08 13:22:12 -0500

    fix crt0

--- libraries/eosiolib/tester/crt0.cpp
+++ libraries/eosiolib/tester/crt0.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char** argv);
 
-extern "C" __attribute__((eosio_wasm_entry)) void initialize() {}
+extern "C" __attribute__((eosio_wasm_entry)) void initialize(int64_t, int64_t, int64_t) {}
 extern "C" __attribute__((eosio_wasm_entry)) int start(void (*f)()) {
   std::vector<std::string> args = eosio::get_args();
    char buf[] = "eosio-tester";
