2f083450045b5ee61485c9e15fc1667bd7cd1429
Ping Liu <ping.liu@block.one>
2021-11-03 16:30:30 -0400

    Add wasm abi files of nested containers and proofread comments

--- tests/toolchain/build-pass/tupletestkv.cpp
+++ tests/toolchain/build-pass/tupletestkv.cpp
@@ -1,19 +1,14 @@
 /* Verify the support of nested containers involving std::tuple<Ts...> for the new Key-value table eosio::kv::map
- * This tupletestkv.cpp can be  regarded as a continuation of ../nestcontn2kv.cpp
+ * This tupletestkv.cpp can be  regarded as a continuation of nestcontn2kv.cpp
  * For each action, an example regarding how to use the action with the cleos command line is given.
  *
  * Important Remarks:
  *      1) The input of a std::tuple<T0,T1,T2> via cleos uses [ele0, ele1, ele2], it is the same as the input of a vector/set
- *      2) However the input formats of settm,settp are different from those of corresponding setvm, setvp in nestcontn2a.cpp
- *      3) More importantly, vector<optional<T> > and set<optional<T> >  are NOT yet supported as shown in nestcontn2a.cpp,
- *         BUT tuple of optional<T> is supported as shown here in this tupletest.cpp
+ *      2) However the input formats of settm,settp are different from those of corresponding setvm, setvp in nestcontn2kv.cpp
  *
  * Expected printout:
  *      For each setx action, the printed result on the cleos console is given in its corresponding prntx action.
  */
-
-
-//--- You might need to verify some eosio-defined types such as eosio::asset can be the values of kv::map
 
 #include <eosio/eosio.hpp>
 #include <eosio/map.hpp>
