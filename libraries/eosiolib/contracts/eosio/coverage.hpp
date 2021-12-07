5b3c7ef36c3d0db96ca8a4ad8a68292d89b4f6b1
Patrick Raphael <patrick.raphael@block.one>
2021-12-07 16:26:34 -0500

    fix static_cast

--- libraries/eosiolib/contracts/eosio/coverage.hpp
+++ libraries/eosiolib/contracts/eosio/coverage.hpp
@@ -33,19 +33,19 @@ namespace eosio {
    };
 
    inline void coverage_inc_fun_cnt( name code, uint32_t file_num, uint32_t func_num ) {
-       internal_use_do_not_use::coverage_inc_fun_cnt(static_cast<capi_name>code, file_num, func_num);
+       internal_use_do_not_use::coverage_inc_fun_cnt(static_cast<capi_name>(code), file_num, func_num);
    }
 
    inline void coverage_inc_line_cnt( name code, uint32_t file_num, uint32_t line_num ) {
-       internal_use_do_not_use::coverage_inc_line_cnt(static_cast<capi_name>code, file_num, line_num);
+       internal_use_do_not_use::coverage_inc_line_cnt(static_cast<capi_name>(code), file_num, line_num);
    }
 
    inline uint32_t coverage_get_fun_cnt( name code, uint32_t file_num, uint32_t func_num ) {
-       return internal_use_do_not_use::coverage_get_fun_cnt(static_cast<capi_name>code, file_num, func_num);
+       return internal_use_do_not_use::coverage_get_fun_cnt(static_cast<capi_name>(code), file_num, func_num);
    }
 
    inline uint32_t coverage_get_line_cnt( name code, uint32_t file_num, uint32_t line_num ) {
-       return internal_use_do_not_use::coverage_get_line_cnt(static_cast<capi_name>code, file_num, line_num);
+       return internal_use_do_not_use::coverage_get_line_cnt(static_cast<capi_name>(code), file_num, line_num);
    }    
 
    inline void coverage_dump() {
