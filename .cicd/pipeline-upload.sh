9eb30217da24b6d1519b841e1c72f6dd641c1be1
adam.rider <adam.rider-ext@block.one>
2022-01-13 09:13:04 -0600

    Comment out MACOS_10_14 steps.

--- .cicd/pipeline-upload.sh
+++ .cicd/pipeline-upload.sh
@@ -3,7 +3,7 @@ set -eou pipefail
 
 # attach pipeline documentation
 echo '+++ :md: Attaching Documentation'
-export DOCS_URL="https://github.com/EOSIO/eosio.cdt/blob/${BUILDKITE_COMMIT:-master}/.cicd/README.md"
+export DOCS_URL="https://github.com/b1-as/taurus-cdt/blob/${BUILDKITE_COMMIT:-master}/.cicd/README.md"
 export RETRY="$(buildkite-agent meta-data get pipeline-upload-retries --default '0')"
 buildkite-agent meta-data set pipeline-upload-retries "$(( $RETRY + 1 ))"
 if [[ "$BUILDKITE" == 'true' && "$RETRY" == '0' ]]; then
@@ -12,7 +12,6 @@ if [[ "$BUILDKITE" == 'true' && "$RETRY" == '0' ]]; then
 elif [[ "$BUILDKITE" == 'true' ]]; then
     printf "Skipping \033]1339;url=$DOCS_URL;content=documentation\a upload for job retry number $RETRY.\n"
 fi
-export MACOS_10_14_TAG="eosio-cdt-macos-10.14-$(sha1sum .cicd/platforms/macos-10.14.sh | awk '{print $1}')"
 export MACOS_10_15_TAG="eosio-cdt-macos-10.15-$(sha1sum .cicd/platforms/macos-10.15.sh | awk '{print $1}')"
 # pipeline upload
 echo '+++ :pipeline_upload: Deploying Pipeline Steps'
