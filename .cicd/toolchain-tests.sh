e0e9d7b4ce7633c02a3d68d72ee127f2bea0fa20
adam.rider <adam.rider-ext@block.one>
2022-01-17 12:03:57 -0600

    Fix docker login

--- .cicd/toolchain-tests.sh
+++ .cicd/toolchain-tests.sh
@@ -4,6 +4,12 @@ set -eo pipefail
 
 mkdir -p $BUILD_DIR
 
+DOCKER_REPO="blockone-b1fs-b1x-docker-dev-local.jfrog.io"
+DOCKER_LOGIN_REPO="https://${DOCKER_REPO}"
+
+echo "login to artifactory"
+echo $ARTIFACTORY_PASSWORD | docker login $DOCKER_LOGIN_REPO -u $ARTIFACTORY_USERNAME --password-stdin
+
 if [[ $(uname) == 'Darwin' ]]; then
 
     # You can't use chained commands in execute
@@ -31,6 +37,11 @@ else # Linux
         done < "$BUILDKITE_ENV_FILE"
     fi
 
-    eval docker run $ARGS $evars $FULL_TAG bash -c \"$COMMANDS\"
+    DOCKER_PULL="docker pull $DOCKER_REPO/$FULL_TAG"
+    echo "$ $DOCKER_PULL"
+    eval $DOCKER_PULL
+    echo "Done with pull"
+
+    eval docker run $ARGS $evars $DOCKER_REPO/$FULL_TAG bash -c \"$COMMANDS\"
 
 fi
