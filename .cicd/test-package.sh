8717b7df2227421646e0f59bc19a459475810206
adam.rider <adam.rider-ext@block.one>
2022-01-18 17:15:02 -0600

    remove extra command

--- .cicd/test-package.sh
+++ .cicd/test-package.sh
@@ -14,9 +14,6 @@ DOCKER_PULL="docker pull $DOCKER_REPO/$IMAGE"
 echo "$ $DOCKER_PULL"
 eval $DOCKER_PULL
 echo "Done with pull"
-echo "eval docker run $ARGS $evars $DOCKER_REPO/$FULL_TAG bash -c \"$COMMANDS\" "
-eval docker run $ARGS $evars $DOCKER_REPO/$FULL_TAG bash -c \"$COMMANDS\"
-echo "done with eval docker run..."
 echo "docker run --rm -v "$(pwd):/eos" -w '/eos' -it $DOCKER_REPO/$IMAGE ./.cicd/test-package.run.sh"
 docker run --rm -v "$(pwd):/eos" -w '/eos' -it $DOCKER_REPO/$IMAGE ./.cicd/test-package.run.sh
 echo "done with docker run..."
