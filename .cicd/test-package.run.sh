08e1d0a3352759a7b5f0bbefe1dbe6de00556757
adam.rider <adam.rider-ext@block.one>
2022-01-19 14:55:42 -0600

    Adding variable SKIP_PACKAGE_BUILDER_TEST

--- .cicd/test-package.run.sh
+++ .cicd/test-package.run.sh
@@ -7,25 +7,13 @@ echo '+++ :minidisc: Installing EOSIO CDT'
 
 if [[ $(apt-get --version 2>/dev/null) ]]; then # debian family packaging
     perform 'apt-get update'
-    perform 'apt-get install -y /eosio.cdt/*.deb'
+    perform 'apt-get install -y /eos/*.deb'
 elif [[ $(yum --version 2>/dev/null) ]]; then # RHEL family packaging
     perform 'yum check-update || :'
-    perform 'yum install -y /eosio.cdt/*.rpm'
-elif [[ $(brew --version 2>/dev/null) ]]; then # homebrew packaging
-    perform 'brew update'
-    perform 'mkdir homebrew-eosio.cdt'
-    perform 'git init homebrew-eosio.cdt'
-    perform 'cp *.rb homebrew-eosio.cdt'
-    perform "sed -i.bk -e 's/url \".*\"/url \"http:\/\/127.0.0.1:7800\"/' homebrew-eosio.cdt/*.rb"
-    perform "pushd homebrew-eosio.cdt && git add *.rb && git commit -m 'test it!' && popd"
-    perform "brew tap eosio/eosio homebrew-eosio.cdt"
-    perform '{ python3 -m http.server 7800 & } && export HTTP_SERVER_PID=$!'
-    perform 'sleep 20s'
-    perform 'brew install eosio.cdt'
-    perform 'kill $HTTP_SERVER_PID'
+    perform 'yum install -y /eos/*.rpm'
 else
     echo 'ERROR: Package manager not detected!'
     exit 3
 fi
 
-eosio-cpp --version
+taurus-cpp --version
