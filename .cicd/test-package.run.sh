7041894d7ff9d568db99ab50203281653e891f3d
adam.rider <adam.rider-ext@block.one>
2022-01-20 19:40:46 -0600

    Revert names in test-package.run.sh

--- .cicd/test-package.run.sh
+++ .cicd/test-package.run.sh
@@ -13,15 +13,15 @@ elif [[ $(yum --version 2>/dev/null) ]]; then # RHEL family packaging
     perform 'yum install -y /eos/*.rpm'
 elif [[ $(brew --version 2>/dev/null) ]]; then # homebrew packaging
     perform 'brew update'
-    perform 'mkdir homebrew-taurus.cdt'
-    perform 'git init homebrew-taurus.cdt'
-    perform 'cp *.rb homebrew-taurus.cdt'
-    perform "sed -i.bk -e 's/url \".*\"/url \"http:\/\/127.0.0.1:7800\"/' homebrew-taurus.cdt/*.rb"
-    perform "pushd homebrew-taurus.cdt && git add *.rb && git commit -m 'test it!' && popd"
-    perform "brew tap taurus/taurus homebrew-taurus.cdt"
+    perform 'mkdir homebrew-eosio.cdt'
+    perform 'git init homebrew-eosio.cdt'
+    perform 'cp *.rb homebrew-eosio.cdt'
+    perform "sed -i.bk -e 's/url \".*\"/url \"http:\/\/127.0.0.1:7800\"/' homebrew-eosio.cdt/*.rb"
+    perform "pushd homebrew-eosio.cdt && git add *.rb && git commit -m 'test it!' && popd"
+    perform "brew tap eosio/eosio homebrew-eosio.cdt"
     perform '{ python3 -m http.server 7800 & } && export HTTP_SERVER_PID=$!'
     perform 'sleep 20s'
-    perform 'brew install taurus.cdt'
+    perform 'brew install eosio.cdt'
     perform 'kill $HTTP_SERVER_PID'
 else
     echo 'ERROR: Package manager not detected!'
